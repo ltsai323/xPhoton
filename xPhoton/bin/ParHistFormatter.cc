5109f5e86790a4b6757b1029f2ca9c825a2a76e1
ltsai323 <johnson20050@gmail.com>
2021-05-14 17:28:39 +0800

    add chIso cut

--- xPhoton/bin/ParHistFormatter.cc
+++ xPhoton/bin/ParHistFormatter.cc
@@ -87,6 +87,7 @@ int main( int argc, char* argv[])
         float pt=data.Float(var::recoPt);
         hists.Fill("pt", pt);
         if ( !(pt > 185. && pt < 190.) ) continue;
+        if ( data.Float(var::chIsoRaw) > 2 ) continue;
 
 
 
