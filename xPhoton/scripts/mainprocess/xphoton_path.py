f467023f3a52ebe638295fac868fe8e4b83e121b
ltsai323 <johnson20050@gmail.com>
2021-06-06 10:56:24 +0800

    rename myrun

--- xPhoton/scripts/mainprocess/xphoton_path.py
+++ xPhoton/scripts/mainprocess/xphoton_path.py
@@ -3,7 +3,6 @@ import re
 from sys import argv, stdout, stderr, exit
 from optparse import OptionParser
 from xPhoton.xPhoton.Managers.ExecuteMgr import ExecMgr, FileNameConverter
-from xPhoton.xPhoton.Managers.ExecuteMgr import prefix_local
 from xPhoton.xPhoton.Managers import LogMgr
 
 import os
@@ -12,24 +11,31 @@ import commands
 # usage :
 #    ./this.py filepath.txt
 #    ./this.py filepath.txt path.txt
+bashcommand='xrdcp %s /tmp/running.root; exec_xPhotonRunner /tmp/running.root %d' # download file first
+fileprefix='root://se01.grid.nchc.org.tw/'
+
+#bashcommand='exec_xPhotonRunner %s %d'
 
 
-def checkarg(inputArg): return inputArg[1] if len(inputArg) > 1 else None
+def checkarg(inputArg):
+    if len(inputArg) < 2:               raise IOError('Input a text path!')
+    if '.root' == inputArg[1][-5:]:     raise IOError('Wrong input : root file is not supported')
+    if not os.path.isfile(inputArg[1]): raise IOError('input file not found')
+    return inputArg[1]
 
-def nlines(filepath): return sum( 1 for line in open(filepath,'r') )
 def ExecSample(execmgr, pathmgr):
     print 'no input file detected. Use internal example'
     # my new code
     lines=[
-        '~/ReceivedFile/GJet/test/ggtree_datasample.root'
+'/cms/store/user/ltsai/ggAnalysis/2016miniAODv3/jetSubVtxInfo/SinglePhoton/crab_data_Run2016B_94XReReco_ver2v1/210308_080300/0000/ggtree_data_1.root',
+        #'~/ReceivedFile/GJet/test/ggtree_datasample.root'
     ]
     for rootFile in lines:
         execmgr.execCommand( pathmgr.GetPath(rootFile),pathmgr.GetFileID(rootFile) )
-
-def ExecByFile(execmgr,pathmgr, textpath):
+def ExecByInputPath(execmgr,pathmgr, textpath):
     print 'input file used : ' + textpath
     with open(textpath, 'r') as ifile:
-        tot=nlines(filename)
+        tot=nlines(textpath)
         for i, remoteDir in enumerate( ifile.readlines() ):
             if i%100 == 0: print '-- %d th / %d -- file processed' % (i, tot)
             mylog.debug(' orig file : %s' % remoteDir )
@@ -47,9 +53,8 @@ if __name__ == '__main__':
 
     txtpath = checkarg(argv)
     cmd=ExecMgr()
-    cmd.SetGeneralCommand('exec_xPhotonRunner %s %d')
-    pathcontroller=FileNameConverter(prefix_local)
-    #pathcontroller=FileNameConverter('root://se01.grid.nchc.org.tw/')
+    cmd.SetGeneralCommand(bashcommand)
+    pathcontroller=FileNameConverter(fileprefix)
 
     if txtpath:
         ExecByInputPath(cmd, pathcontroller, txtpath)
