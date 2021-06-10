81ab54927ead9fd12709bdf728e24b6776f70dd4
ltsai323 <johnson20050@gmail.com>
2021-06-10 09:18:22 +0800

    modify LogMgr output format

--- xPhoton/interface/LogMgr.h
+++ xPhoton/interface/LogMgr.h
@@ -4,12 +4,13 @@
 
 // current setting
 //#define __print_debug__
-#define __print_warning__
+#define __print_info__
+//#define __print_warning__
 
 
 
 #ifdef __print_debug__
-    #define LOG_DEBUG(format, args...)   fprintf(stderr, "-debug-    %s  : " format "\n", __PRETTY_FUNCTION__,  ##args)
+    #define LOG_DEBUG(format, args...)     fprintf(stderr, "-debug-    %s  \n  --->  " format "\n", __PRETTY_FUNCTION__,  ##args)
     #define __print_info__
     #define __print_critical__
     #define __print_warning__
@@ -19,7 +20,7 @@
 #endif
 
 #ifdef __print_info__
-    #define LOG_INFO(format, args...)    fprintf(stderr, "-info-     %s  : " format "\n", __PRETTY_FUNCTION__,  ##args)
+    #define LOG_INFO(format, args...)      fprintf(stderr, "-info-     %s  \n  --->  " format "\n", __PRETTY_FUNCTION__,  ##args)
     #define __print_critical__
     #define __print_warning__
     #define __print_fatal__
@@ -28,7 +29,7 @@
 #endif
 
 #ifdef __print_warning__
-    #define LOG_WARNING(format, args...) fprintf(stderr, "-warning-  %s  : " format "\n", __PRETTY_FUNCTION__,  ##args)
+    #define LOG_WARNING(format, args...)   fprintf(stderr, "-warning-  %s  \n  --->  " format "\n", __PRETTY_FUNCTION__,  ##args)
     #define __print_fatal__
     #define __print_critical__
 #else
@@ -36,14 +37,14 @@
 #endif
 
 #ifdef __print_fatal__
-    #define LOG_FATAL(format, args...)  {fprintf(stderr, "-fatal-    %s  : " format "\n", __PRETTY_FUNCTION__,  ##args); exit(1); }
+    #define LOG_FATAL(format, args...)    {fprintf(stderr, "-fatal-    %s  \n  --->  " format "\n", __PRETTY_FUNCTION__,  ##args); exit(1); }
     #define __print_critical__
 #else
     #define LOG_FATAL(format, args...)
 #endif
 
 #ifdef __print_critical__
-    #define LOG_CRITICAL(format, args...) {fprintf(stderr, "-critical- %s  : " format "\n", __PRETTY_FUNCTION__,  ##args); exit(2); }
+    #define LOG_CRITICAL(format, args...) {fprintf(stderr, "-critical- %s  \n  --->  " format "\n", __PRETTY_FUNCTION__,  ##args); exit(2); }
 #else
     #define LOG_CRITICAL(format, args...)
 #endif
