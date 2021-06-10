4296465c4d4d2b364aa74c79f1fc6b4750f5189d
ltsai323 <johnson20050@gmail.com>
2021-06-10 08:53:12 +0800

    LogMgr : move critical to be the most important one.

--- xPhoton/interface/LogMgr.h
+++ xPhoton/interface/LogMgr.h
@@ -4,7 +4,7 @@
 
 // current setting
 //#define __print_debug__
-#define __print_info__
+#define __print_warning__
 
 
 
@@ -27,25 +27,25 @@
     #define LOG_INFO(format, args...)
 #endif
 
-#ifdef __print_critical__
-    #define LOG_CRITICAL(format, args...)fprintf(stderr, "-critical- %s  : " format "\n", __PRETTY_FUNCTION__,  ##args)
-    #define __print_warning__
-    #define __print_fatal__
-#else
-    #define LOG_INFO(format, args...)
-#endif
-
 #ifdef __print_warning__
     #define LOG_WARNING(format, args...) fprintf(stderr, "-warning-  %s  : " format "\n", __PRETTY_FUNCTION__,  ##args)
     #define __print_fatal__
+    #define __print_critical__
 #else
     #define LOG_WARNING(format, args...)
 #endif
 
 #ifdef __print_fatal__
     #define LOG_FATAL(format, args...)  {fprintf(stderr, "-fatal-    %s  : " format "\n", __PRETTY_FUNCTION__,  ##args); exit(1); }
+    #define __print_critical__
 #else
     #define LOG_FATAL(format, args...)
+#endif
+
+#ifdef __print_critical__
+    #define LOG_CRITICAL(format, args...) {fprintf(stderr, "-critical- %s  : " format "\n", __PRETTY_FUNCTION__,  ##args); exit(2); }
+#else
+    #define LOG_CRITICAL(format, args...)
 #endif
 
 
