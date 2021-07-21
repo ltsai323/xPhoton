2300a44d6734417ee3060a20ef23051532cec29d
ltsai323 <johnson20050@gmail.com>
2021-07-22 00:15:23 +0800

    AppendEventInfo : remove BTag part. And add integrated gen weight in to denominator. Also, keep all histograms into new files

--- xPhoton/bin/AppendEventInfo.cc
+++ xPhoton/bin/AppendEventInfo.cc
@@ -1,5 +1,6 @@
 #include <TTree.h>
 #include <TFile.h>
+#include <TKey.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
@@ -8,33 +9,38 @@
 #include "xPhoton/xPhoton/interface/LogMgr.h"
 // usage :
 //   ./exec_thisfile input.root outfile.root 3.283
+/*
 bool useDeepCSV = false;
 bool useDeepJet = false;
+*/
 
 void PrintHelp()
 {
-    printf("---------------------------------------------\n");
-    printf("------ Used to update cross section. --------\n");
-    printf("------ Arguments :                   --------\n");
-    printf("------ 1. cross section value of MC  --------\n");
-    printf("------ 2. input root file            --------\n");
-    printf("------ 3. output root file           --------\n");
-    printf("---------------------------------------------\n");
-    printf("------ Feature : 1. Add Xsec info    --------\n");
-    printf("------           2. Scale Factor of  --------\n");
-    printf("------              jet to calibrate --------\n");
-    printf("------              BTagging.        --------\n");
-    printf("---------------------------------------------\n");
+    printf("------------------------------------------------\n");
+    printf("------ Used to update cross section.    --------\n");
+    printf("------ Arguments :                      --------\n");
+    printf("------ 1. cross section value of MC     --------\n");
+    printf("------ 2. integrated gen weights in MC  --------\n");
+    printf("------ 3. input root file               --------\n");
+    printf("------ 4. output root file              --------\n");
+    printf("------------------------------------------------\n");
+    printf("------ Feature : 1. Add Xsec info       --------\n");
+    printf("------           2. xsweight = cross    --------\n");
+    printf("------              section * genWeight --------\n");
+    printf("------              / integrated gen W  --------\n");
+    printf("------------------------------------------------\n");
 }
 float GetWeight(const char* argv[])
 { return atof(argv[1]); }
+float GetIntegratedGenWeight(const char* argv[])
+{ return atof(argv[2]); }
 const char* GetInputFile(const char* argv[])
-{ return argv[2]; }
-const char* GetOutputFile(const char* argv[])
 { return argv[3]; }
+const char* GetOutputFile(const char* argv[])
+{ return argv[4]; }
 void CheckArgs(int argc, const char* argv[])
 {
-    if ( argc != 4 )
+    if ( argc != 5 )
     { PrintHelp(); throw std::invalid_argument(" --- Need 3 input arguments ---\n"); }
 }
 
@@ -46,18 +52,22 @@ int main(int argc, const char* argv[])
     const char* iFile = GetInputFile(argv);
     const char* oFile = GetOutputFile(argv);
     const float new_xs = GetWeight(argv);
+    const float integratedGenWeight = GetIntegratedGenWeight(argv);
     std::cout << "in file : " << iFile << std::endl;
     std::cout << "out file : " << oFile << std::endl;
     std::cout << "in x-sec : " << new_xs << std::endl;
+    std::cout << "input integrated genweights : " << integratedGenWeight << std::endl;
     
 
     TFile* iF = TFile::Open(iFile);
     TTree* iT = (TTree*) iF->Get("t");
     iT->SetBranchStatus("xsweight", 0);
+
     
     TFile* oF = new TFile(oFile,"recreate");
     oF->cd();
     TTree* oT = (TTree*) iT->CloneTree(0);
+    /*
     BTaggingMgr btagCalibsDeepCSV;
     BTaggingMgr btagCalibsDeepFlavour;
     BTaggingMgr btagCalibsDeepFlavour_JESReduced;;
@@ -70,15 +80,20 @@ int main(int argc, const char* argv[])
     if ( useDeepCSV ) btagCalibsDeepCSV.RegisterSystTypes();
     if ( useDeepJet ) btagCalibsDeepFlavour.RegisterSystTypes();
     if ( useDeepJet ) btagCalibsDeepFlavour_JESReduced.RegisterSystTypes();
+    */
 
     Float_t xsweight;
-    //oT->SetBranchStatus("xsweight", 0);
     oT->Branch("xsweight", &xsweight, "xsweight/F");
 
+    /*
     Float_t jetPt, jetEta;
     Float_t deepcsv_b, deepcsv_bb;
     Float_t deepjet_b, deepjet_bb, deepjet_lepb;
     Int_t flavour;
+    */
+    Float_t genweight;
+    iT->SetBranchAddress("genWeight", &genweight);
+    /*
     iT->SetBranchAddress("jetPt",     &jetPt);
     iT->SetBranchAddress("jetEta",    &jetEta);
     iT->SetBranchAddress("jetHadFlvr", &flavour);
@@ -91,6 +106,7 @@ int main(int argc, const char* argv[])
     if ( useDeepCSV ) btagCalibsDeepCSV.RegBranch(oT);
     if ( useDeepJet ) btagCalibsDeepFlavour.RegBranch(oT);
     if ( useDeepJet ) btagCalibsDeepFlavour_JESReduced.RegBranch(oT);
+    */
 
     xsweight = new_xs;
     
@@ -99,6 +115,9 @@ int main(int argc, const char* argv[])
     {
         iT->GetEntry(ievt);
 
+        xsweight = new_xs * genweight / integratedGenWeight;
+
+        /*
         if ( jetPt > 10 )
         {
             if ( useDeepCSV ) btagCalibsDeepCSV.InitVars();
@@ -108,11 +127,18 @@ int main(int argc, const char* argv[])
             if ( useDeepJet ) btagCalibsDeepFlavour.FillWeightToEvt(jetPt,jetEta, flavour, deepjet_b+deepjet_bb+deepjet_lepb);
             if ( useDeepJet ) btagCalibsDeepFlavour_JESReduced.FillWeightToEvt(jetPt,jetEta, flavour, deepjet_b+deepjet_bb+deepjet_lepb);
         }
+        */
 
         oT->Fill();
     }
 
     oT->Write();
+    TIter keyList(iF->GetListOfKeys());
+    TKey *key;
+    while ((key = (TKey*)keyList())) {
+        if ( (key->GetName()) == "t" ) continue;
+       key->ReadObj()->Write();
+    }
     oF->Close();
     iF->Close();
 
--- xPhoton/bin/py.AppendEventInfo.madgraphMLM.py
+++ xPhoton/bin/py.AppendEventInfo.madgraphMLM.py
@@ -2,59 +2,105 @@
 summaryfile='../data/summaryJson/summary_sigMC_madgraph.json'
 fileDict={
   "GJets_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
-#    'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-100To200_13TeV-madgraphMLM-pythia8_v3-v2.root',
-#    'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-100To200_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
     'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-100To200_13TeV-madgraphMLM-pythia8_v3-v2.root',
     'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-100To200_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
   },
   "GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
-#    'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-200To400_13TeV-madgraphMLM-pythia8_v3-v2.root',
-#    'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-200To400_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
     'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-200To400_13TeV-madgraphMLM-pythia8_v3-v2.root',
     'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-200To400_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
   },
   "GJets_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
-#    'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-400To600_13TeV-madgraphMLM-pythia8_v3-v2.root',
-#    'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-400To600_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
     'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-400To600_13TeV-madgraphMLM-pythia8_v3-v2.root',
     'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-400To600_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
   },
   "GJets_HT-40To100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
-#    'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-40To100_13TeV-madgraphMLM-pythia8_v3-v2.root',
-#    'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-40To100_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
     'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-40To100_13TeV-madgraphMLM-pythia8_v3-v2.root',
     'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-40To100_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
   },
   "GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
-#    'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-600ToInf_13TeV-madgraphMLM-pythia8_v3-v2.root',
-#    'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/origfiles/crab_sigMC_HT-600ToInf_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
     'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-600ToInf_13TeV-madgraphMLM-pythia8_v3-v2.root',
     'v3_ext1-v2':'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-600ToInf_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
   }
 }
-testDict={
+processedDict={
   "GJets_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
-    'v3-v2'     :'/home/ltsai/ReceivedFile/GJet/madgraphMLM/crab_sigMC_HT-100To200_13TeV-madgraphMLM-pythia8_v3-v2.root',
-    }
+    'v3-v2'     :'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-100To200_13TeV-madgraphMLM-pythia8_v3-v2.root',
+    'v3_ext1-v2':'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-100To200_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
+  },
+  "GJets_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
+    'v3-v2'     :'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-200To400_13TeV-madgraphMLM-pythia8_v3-v2.root',
+    'v3_ext1-v2':'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-200To400_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
+  },
+  "GJets_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
+    'v3-v2'     :'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-400To600_13TeV-madgraphMLM-pythia8_v3-v2.root',
+    'v3_ext1-v2':'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-400To600_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
+  },
+  "GJets_HT-40To100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
+    'v3-v2'     :'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-40To100_13TeV-madgraphMLM-pythia8_v3-v2.root',
+    'v3_ext1-v2':'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-40To100_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
+  },
+  "GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
+    'v3-v2'     :'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-600ToInf_13TeV-madgraphMLM-pythia8_v3-v2.root',
+    'v3_ext1-v2':'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-600ToInf_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
   }
+}
+testDict={
+  "GJets_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8": {
+  'v3-v2'      :'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-600ToInf_13TeV-madgraphMLM-pythia8_v3-v2.root',
+  'v3_ext1-v2' :'/home/ltsai/Work/workspaceGammaPlusJet/xPhoton/xPhoton/test/storeroot/crab_sigMC_HT-600ToInf_13TeV-madgraphMLM-pythia8_v3_ext1-v2.root',
+  }
+}
 import os
 from xPhoton.xPhoton.AppendEventInfo import FindXSInfo, nodir, ShowPD, ShowDetail
 from xPhoton.xPhoton.Managers.LogMgr import InitLogger, GetLogger
+mylog=GetLogger(__name__)
 
-def executeCommand( xsweight, inputfile ):
+#from xPhoton.xPhoton.AppendEventInfo_IntegratedGenWeights import IntegratedGenWeights
+import ROOT
+def IntegratedGenWeights(filename):
+    f=ROOT.TFile.Open(filename)
+    nt=f.Get('genweightsummary')
+
+    Sum=0
+    for a in nt:
+        Sum+=a.sumupgenweight
+    return Sum
+
+def MergeNeeded( filedict ):
+    if len( filedict.keys() ) > 1:
+        return True
+    return False
+
+
+def MergeOutputs( primarydataset, inputlist ):
+    inputfiles=[ fpath for ver, fpath in inputlist.iteritems() ]
+    if len(inputfiles) == 1:
+        os.system('mv tmp/%s updated/%s.root' % nodir(inputfiles[0]), primarydataset )
+    elif len(inputfiles) > 1:
+        os.system('hadd updated/%s.root %s' % (primarydataset, ' '.join( ['tmp/'+ nodir(fpath) for fpath in inputfiles] )))
+    else:
+        mylog.warning('nothing found in category %s' % primarydataset)
+
+def executeCommandToTmp( xsweight_, integratedGenWeight_, inputfile_ ):
     execfile='./exe.AppendEventInfo'
-    os.system( '%s %.10f %s updated/%s' %( execfile, xsweight, inputfile, nodir(inputfile) ) )
+    os.system( '%s %.10f %f %s tmp/%s' %( execfile, xsweight_, integratedGenWeight_, inputfile_, nodir(inputfile_) ) )
 
 if __name__ == '__main__':
     InitLogger(level='info')
     mylog=GetLogger(__name__)
     newdir='updated'
     if not os.path.exists(newdir) or not os.path.isdir(newdir): os.mkdir(newdir)
+    if not os.path.exists('tmp') or not os.path.isdir(newdir): os.mkdir('tmp')
+
     ShowPD(summaryfile)
     ShowDetail(summaryfile)
     version=''
-    for pd, info in fileDict.iteritems():
-    #for pd, info in testDict.iteritems():
+
+    targetDict=processedDict
+    integratedgenweights = { pd:sum( [IntegratedGenWeights(fpath) for ver,fpath in info.iteritems()] )
+            for pd, info in targetDict.iteritems() }
+
+    for pd, info in targetDict.iteritems():
         for ver, rootfile in info.iteritems():
             xs,xserr,xsunit=FindXSInfo(pd,ver,summaryfile)
             if   xsunit=='pb':
@@ -64,4 +110,5 @@ if __name__ == '__main__':
             else :
                 mylog.error( 'recorded Xsection in "%s" unit in file %s. Which is not "pb" or "fb". Need to convert it manually!' % (xsunit, summaryfile) )
             #print xs
-            executeCommand( xs, rootfile )
+            executeCommandToTmp( xs, integratedgenweights[pd], rootfile )
+        MergeOutputs(pd, info)
