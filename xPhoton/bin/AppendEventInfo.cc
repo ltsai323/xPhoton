bc2bd61010e84c6b6f4fc3de3fe944300fa5485c
ltsai323 <johnson20050@gmail.com>
2021-07-22 17:46:51 +0800

    AppendEventInfo : add data luminosity

--- xPhoton/bin/AppendEventInfo.cc
+++ xPhoton/bin/AppendEventInfo.cc
@@ -1,6 +1,11 @@
 #include <TTree.h>
 #include <TFile.h>
 #include <TKey.h>
+#include <TROOT.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TNtupleD.h>
+#include <TNtuple.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
@@ -8,41 +13,46 @@
 #include "xPhoton/xPhoton/interface/BTaggingMgr.h"
 #include "xPhoton/xPhoton/interface/LogMgr.h"
 // usage :
-//   ./exec_thisfile input.root outfile.root 3.283
-/*
-bool useDeepCSV = false;
-bool useDeepJet = false;
-*/
+//   ./exec_thisfile 3.14e5 200 35.9 input.root outfile.root
 
 void PrintHelp()
 {
-    printf("------------------------------------------------\n");
-    printf("------ Used to update cross section.    --------\n");
-    printf("------ Arguments :                      --------\n");
-    printf("------ 1. cross section value of MC     --------\n");
-    printf("------ 2. integrated gen weights in MC  --------\n");
-    printf("------ 3. input root file               --------\n");
-    printf("------ 4. output root file              --------\n");
-    printf("------------------------------------------------\n");
-    printf("------ Feature : 1. Add Xsec info       --------\n");
-    printf("------           2. xsweight = cross    --------\n");
-    printf("------              section * genWeight --------\n");
-    printf("------              / integrated gen W  --------\n");
-    printf("------------------------------------------------\n");
+    printf("--------------------------------------------------\n");
+    printf("------ Used to update cross section.      --------\n");
+    printf("------ Arguments :                        --------\n");
+    printf("------ 1. cross section value of MC       --------\n");
+    printf("------ 2. integrated gen weights in MC    --------\n");
+    printf("------ 3. input integrated luminosity     --------\n");
+    printf("------ 4. input root file                 --------\n");
+    printf("------ 5. output root file                --------\n");
+    printf("--------------------------------------------------\n");
+    printf("------ Feature : 1. Add Xsec info         --------\n");
+    printf("------           2. xsweight = xs         --------\n");
+    printf("------           3. overall genweight =   --------\n");
+    printf("------              integrated gen weight --------\n");
+    printf("------           4. lumi = data luminosity--------\n");
+    printf("------           5. mcweight = luminosity --------\n");
+    printf("------              * cross section of MC --------\n");
+    printf("------              * gen weight each evt --------\n");
+    printf("------              / integrated gen W    --------\n");
+    printf("--------------------------------------------------\n");
 }
 float GetWeight(const char* argv[])
 { return atof(argv[1]); }
 float GetIntegratedGenWeight(const char* argv[])
 { return atof(argv[2]); }
+float GetIntegratedLuminosity(const char* argv[])
+{ return atof(argv[3]); }
 const char* GetInputFile(const char* argv[])
-{ return argv[3]; }
-const char* GetOutputFile(const char* argv[])
 { return argv[4]; }
+const char* GetOutputFile(const char* argv[])
+{ return argv[5]; }
 void CheckArgs(int argc, const char* argv[])
 {
-    if ( argc != 5 )
-    { PrintHelp(); throw std::invalid_argument(" --- Need 3 input arguments ---\n"); }
+    if ( argc != 6 )
+    { PrintHelp(); throw std::invalid_argument(" --- Need 5 input arguments ---\n"); }
 }
+
 
 
 
@@ -53,10 +63,12 @@ int main(int argc, const char* argv[])
     const char* oFile = GetOutputFile(argv);
     const float new_xs = GetWeight(argv);
     const float integratedGenWeight = GetIntegratedGenWeight(argv);
+    const float integratedLumi = GetIntegratedLuminosity(argv);
     std::cout << "in file : " << iFile << std::endl;
     std::cout << "out file : " << oFile << std::endl;
     std::cout << "in x-sec : " << new_xs << std::endl;
     std::cout << "input integrated genweights : " << integratedGenWeight << std::endl;
+    std::cout << "input integrated luminosity : " << integratedLumi << std::endl;
     
 
     TFile* iF = TFile::Open(iFile);
@@ -67,67 +79,34 @@ int main(int argc, const char* argv[])
     TFile* oF = new TFile(oFile,"recreate");
     oF->cd();
     TTree* oT = (TTree*) iT->CloneTree(0);
-    /*
-    BTaggingMgr btagCalibsDeepCSV;
-    BTaggingMgr btagCalibsDeepFlavour;
-    BTaggingMgr btagCalibsDeepFlavour_JESReduced;;
-
-    if ( useDeepCSV ) btagCalibsDeepCSV.UseAlgorithm( "DeepCSV" );
-    if ( useDeepJet ) btagCalibsDeepFlavour.UseAlgorithm( "DeepFlavour" );
-    if ( useDeepJet ) btagCalibsDeepFlavour_JESReduced.UseAlgorithm( "DeepFlavour_JESReduced" );
-
-
-    if ( useDeepCSV ) btagCalibsDeepCSV.RegisterSystTypes();
-    if ( useDeepJet ) btagCalibsDeepFlavour.RegisterSystTypes();
-    if ( useDeepJet ) btagCalibsDeepFlavour_JESReduced.RegisterSystTypes();
-    */
 
     Float_t xsweight;
-    oT->Branch("xsweight", &xsweight, "xsweight/F");
+    Float_t lumi;
+    Float_t xs;
+    Float_t mcweight;
+    Float_t sumGenWeight;
+    oT->Branch("xsweight", &xsweight, "xsweight/F"); // xsweight is the variable serve for original RS code.
+    oT->Branch("crossSection", &xs, "crossSection/F");
+    oT->Branch("integratedLuminosity", &lumi, "integratedLuminosity/F");
+    oT->Branch("integratedGenWeight", &sumGenWeight, "integratedGenWeight/F");
+    oT->Branch("mcweight", &mcweight, "mcweight/F");
 
-    /*
-    Float_t jetPt, jetEta;
-    Float_t deepcsv_b, deepcsv_bb;
-    Float_t deepjet_b, deepjet_bb, deepjet_lepb;
-    Int_t flavour;
-    */
     Float_t genweight;
+
     iT->SetBranchAddress("genWeight", &genweight);
-    /*
-    iT->SetBranchAddress("jetPt",     &jetPt);
-    iT->SetBranchAddress("jetEta",    &jetEta);
-    iT->SetBranchAddress("jetHadFlvr", &flavour);
-    iT->SetBranchAddress("jetDeepCSVTags_b" ,&deepcsv_b);
-    iT->SetBranchAddress("jetDeepCSVTags_bb",&deepcsv_bb);
 
-    iT->SetBranchAddress("jetDeepFlavourTags_b"   ,&deepjet_b);
-    iT->SetBranchAddress("jetDeepFlavourTags_bb"  ,&deepjet_bb);
-    iT->SetBranchAddress("jetDeepFlavourTags_lepb",&deepjet_lepb);
-    if ( useDeepCSV ) btagCalibsDeepCSV.RegBranch(oT);
-    if ( useDeepJet ) btagCalibsDeepFlavour.RegBranch(oT);
-    if ( useDeepJet ) btagCalibsDeepFlavour_JESReduced.RegBranch(oT);
-    */
-
-    xsweight = new_xs;
+    xs = new_xs;
+    lumi = integratedLumi;
+    sumGenWeight = integratedGenWeight;
+    
     
     unsigned int nevt = iT->GetEntries();
     for ( unsigned int ievt = 0; ievt <= nevt; ++ievt )
     {
         iT->GetEntry(ievt);
 
-        xsweight = new_xs * genweight / integratedGenWeight;
-
-        /*
-        if ( jetPt > 10 )
-        {
-            if ( useDeepCSV ) btagCalibsDeepCSV.InitVars();
-            if ( useDeepJet ) btagCalibsDeepFlavour.InitVars();
-            if ( useDeepJet ) btagCalibsDeepFlavour_JESReduced.InitVars();
-            if ( useDeepCSV ) btagCalibsDeepCSV.FillWeightToEvt(jetPt,jetEta, flavour, deepcsv_b+deepcsv_bb);
-            if ( useDeepJet ) btagCalibsDeepFlavour.FillWeightToEvt(jetPt,jetEta, flavour, deepjet_b+deepjet_bb+deepjet_lepb);
-            if ( useDeepJet ) btagCalibsDeepFlavour_JESReduced.FillWeightToEvt(jetPt,jetEta, flavour, deepjet_b+deepjet_bb+deepjet_lepb);
-        }
-        */
+        xsweight = genweight > 0 ? new_xs : -1.*new_xs;
+        mcweight = new_xs * lumi * genweight / integratedGenWeight;
 
         oT->Fill();
     }
@@ -137,7 +116,18 @@ int main(int argc, const char* argv[])
     TKey *key;
     while ((key = (TKey*)keyList())) {
         if ( (key->GetName()) == "t" ) continue;
-       key->ReadObj()->Write();
+        if ( key->ReadObj()->IsZombie() ) continue;
+        TClass *cl = gROOT->GetClass(key->GetClassName());
+        if (cl->InheritsFrom("TH1"))
+            ((TH1*     )key->ReadObj())->Write();
+        /*
+        if (cl->InheritsFrom("TNtupleD"))
+            ((TNtupleD*)key->ReadObj())->Write();
+            */
+        if (cl->InheritsFrom("TNtuple"))
+            ((TNtuple* )key->ReadObj())->Write();
+        if (cl->InheritsFrom("TH2"))
+            ((TH2*     )key->ReadObj())->Write();
     }
     oF->Close();
     iF->Close();
