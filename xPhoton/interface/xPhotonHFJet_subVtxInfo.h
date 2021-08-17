0d291c15c9e5414d54173f1ef38f026d600ec5ca
ltsai323 <johnson20050@gmail.com>
2021-08-17 21:40:26 +0800

    xPhoton : open BJet variables switch:

--- xPhoton/interface/xPhotonHFJet_subVtxInfo.h
+++ xPhoton/interface/xPhotonHFJet_subVtxInfo.h
@@ -29,7 +29,7 @@ static Int_t genHTcut=0;
 static Int_t gjetSignal=0;
 
 static bool hasSubVtxInfo = true;
-static bool testJetSF = false;
+static bool testJetSF = true;
 static bool ELECTRONVETO = false;
 static bool USEHLT = false;
 
