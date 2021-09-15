e553011a5224d9cc35a84c15d07a596db1d9a04c
ltsai323 <johnson20050@gmail.com>
2021-09-15 10:09:28 +0800

    step4 : recovered executable

---
+++ macros/step4.DrawYield/run.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+# part 1 : fit and draw result and print informations to log
+# part 2 : collect the results and create TTree readable file.
+
+for isEE in 0 1; do
+for jetBin in 0 1 2; do
+root -b > log.ebee_${isEE}_${jetBin} <<EOF
+.L DrawYield.C+
+DrawYield(${isEE},${jetBin})
+EOF
+done
+done
+
+# inclusive gamma measurement : no matter how jet distributed
+echo "ptbin/I:EBEE/I:jetbin/I:bkg/F:bkg_err/F" > data_bkg_leadingpho.dat
+grep "EB bin " log.ebee_0_0 | grep BKG | awk '{print $3, 0, 0, $5, $7}' >> data_bkg_leadingpho.dat
+grep "EE bin " log.ebee_1_0 | grep BKG | awk '{print $3, 1, 0, $5, $7}' >> data_bkg_leadingpho.dat
+grep "EB bin " log.ebee_0_1 | grep BKG | awk '{print $3, 0, 1, $5, $7}' >> data_bkg_leadingpho.dat
+grep "EE bin " log.ebee_1_1 | grep BKG | awk '{print $3, 1, 1, $5, $7}' >> data_bkg_leadingpho.dat
+grep "EB bin " log.ebee_0_2 | grep BKG | awk '{print $3, 0, 2, $5, $7}' >> data_bkg_leadingpho.dat
+grep "EE bin " log.ebee_1_2 | grep BKG | awk '{print $3, 1, 2, $5, $7}' >> data_bkg_leadingpho.dat
+
+echo "ptbin/I:EBEE/I:jetbin/I:yield/F:yield_err/F" > data_yield_leadingpho.dat
+grep "EB bin " log.ebee_0_0 | grep SIG | awk '{print $3, 0, 0, $5, $7}' >> data_yield_leadingpho.dat
+grep "EE bin " log.ebee_1_0 | grep SIG | awk '{print $3, 1, 0, $5, $7}' >> data_yield_leadingpho.dat
+grep "EB bin " log.ebee_0_1 | grep SIG | awk '{print $3, 0, 1, $5, $7}' >> data_yield_leadingpho.dat
+grep "EE bin " log.ebee_1_1 | grep SIG | awk '{print $3, 1, 1, $5, $7}' >> data_yield_leadingpho.dat
+grep "EB bin " log.ebee_0_2 | grep SIG | awk '{print $3, 0, 2, $5, $7}' >> data_yield_leadingpho.dat
+grep "EE bin " log.ebee_1_2 | grep SIG | awk '{print $3, 1, 2, $5, $7}' >> data_yield_leadingpho.dat
+
+# gamma + barrel jet
+echo "ptbin/I:EBEE/I:jetbin/I:bkg/F:bkg_err/F" > data_bkg_endcapJet.dat
+grep "EB bin " log.ebee_0_1 | grep BKG | awk '{print $3, 0, 1, $5, $7}' >> data_bkg_endcapJet.dat
+grep "EE bin " log.ebee_1_1 | grep BKG | awk '{print $3, 1, 1, $5, $7}' >> data_bkg_endcapJet.dat
+
+echo "ptbin/I:EBEE/I:jetbin/I:yield/F:yield_err/F" > data_yield_endcapJet.dat
+grep "EB bin " log.ebee_0_1 | grep SIG | awk '{print $3, 0, 1, $5, $7}' >> data_yield_endcapJet.dat
+grep "EE bin " log.ebee_1_1 | grep SIG | awk '{print $3, 1, 1, $5, $7}' >> data_yield_endcapJet.dat
+
+# gamma + endcap jet
+echo "ptbin/I:EBEE/I:jetbin/I:bkg/F:bkg_err/F" > data_bkg_barrelJet.dat
+grep "EB bin " log.ebee_0_0 | grep BKG | awk '{print $3, 0, 0, $5, $7}' >> data_bkg_barrelJet.dat
+grep "EE bin " log.ebee_1_0 | grep BKG | awk '{print $3, 1, 0, $5, $7}' >> data_bkg_barrelJet.dat
+
+echo "ptbin/I:EBEE/I:jetbin/I:yield/F:yield_err/F" > data_yield_barrelJet.dat
+grep "EB bin " log.ebee_0_0 | grep SIG | awk '{print $3, 0, 0, $5, $7}' >> data_yield_barrelJet.dat
+grep "EE bin " log.ebee_1_0 | grep SIG | awk '{print $3, 1, 0, $5, $7}' >> data_yield_barrelJet.dat
