33d99e402130296866fb6e312e35579ed9c76a77
ltsai323 <johnson20050@gmail.com>
2021-10-07 09:31:42 +0800

    DrawIsoBDT : fakerun.sh updated

--- macros/step3.DrawIsoBDT/fakerun.sh
+++ macros/step3.DrawIsoBDT/fakerun.sh
@@ -2,16 +2,19 @@
 
 #iBin=0
 
-for iBin in {1..9}; do
+for iFile in `ls ../step2.makehistos/storeroot/makehisto_fakedata*.root `; do
+outnametemplate=`echo $iFile | rev | cut -d'/' -f1 | cut -d'.' -f2-10 | rev | cut -d'_' -f2-10`    
+
 for phoPtBin in {0..24}; do
 for phoEtaIdx in {0..1}; do
 for jetEtaIdx in {0..2}; do
 root -b > logs/log_fakesample${iBin}_phoEta${phoEtaIdx}_jetEta${jetEtaIdx}_phoPt${phoPtBin} 2> /dev/null <<EOF
 .L Draw_IsovsBDT.C+
-Draw_IsovsBDT(${phoEtaIdx},${jetEtaIdx},${phoPtBin},1, 14, 20, -10${iBin})
+Draw_IsovsBDT(${iFile},${phoEtaIdx},${jetEtaIdx},${phoPtBin},1, 14, 20)
 EOF
-mv isovsbdt.root iso_${phoEtaIdx}_${jetEtaIdx}_${phoPtBin}_fakesample${iBin}.root
+mv isovsbdt.root iso_${outnametemplate}_${phoEtaIdx}_${jetEtaIdx}_${phoPtBin}.root
 done; done; done
-hadd -f storeroot/isovsbdt_template_fakesample${iBin}.root iso_*.root
+
+hadd -f storeroot/isovsbdt_template_${outnametemplate}.root iso_*.root
 /bin/rm  iso_*.root
 done
