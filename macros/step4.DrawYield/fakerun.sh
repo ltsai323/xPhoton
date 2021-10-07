fb4a6db428875deb2399c1050406bde111b57141
ltsai323 <johnson20050@gmail.com>
2021-10-07 10:01:56 +0800

    DrawYield : Update fakerun.sh and fullrun.sh

--- macros/step4.DrawYield/fakerun.sh
+++ macros/step4.DrawYield/fakerun.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env sh
-# 1. relink isobsbdt_template.root for use script.
+# purpose : Find fake sample and process run.sh
+# 1. relink isovsbdt_template.root for use script.
 # 2. run the script.
 # 3. move outputs into storage
 
 for infile in `echo ../step3.DrawIsoBDT/storeroot/isovsbdt_template_fakesample*.root`;
 do
     outname=`echo $infile | rev | cut -d'/' -f1 | cut -d'.' -f2-10 | rev | cut -d'_' -f3-10`
-    touch isovsbdt_template.root; /bin/rm isovsbdt_template.root
-    ln -s ../step3.DrawIsoBDT/storeroot/isovsbdt_template.root
+    touch isovsbdt_template.root;
+    unlink isovsbdt_template.root || rm isovsbdt_template.root
+    ln -s $infile isovsbdt_template.root
+
+    touch plots ; /bin/rm -r plots
+    mkdir plots
     sh run.sh
 
     mv data_bkg_allphoton.dat    storeroot/${outname}_bkg_allphoton.dat
@@ -16,5 +21,6 @@ do
     mv data_yield_allphoton.dat  storeroot/${outname}_yield_allphoton.dat
     mv data_yield_barrelJet.dat  storeroot/${outname}_yield_barrelJet.dat
     mv data_yield_endcapJet.dat  storeroot/${outname}_yield_endcapJet.dat
+    mv plots storefig/${outname}_plots
     /bin/rm log*
 done
--- macros/step4.DrawYield/fullrun.sh
+++ macros/step4.DrawYield/fullrun.sh
@@ -2,8 +2,13 @@
 # 1. relink isobsbdt_template.root for use script.
 # 2. run the script.
 # 3. move outputs into storage
-touch isovsbdt_template.root; /bin/rm isovsbdt_template.root
-ln -s ../step3.DrawIsoBDT/storeroot/isovsbdt_template.root
+touch isovsbdt_template.root; unlink isovsbdt_template.root || /bin/rm isovsbdt_template.root
+
+ln -s ../step3.DrawIsoBDT/storeroot/isovsbdt_template.root isovsbdt_template.root
+
+touch plots; /bin/rm -r plots/
+mkdir plots
+
 sh run.sh
 mv data_bkg_allphoton.dat    storeroot/data_bkg_allphoton.dat
 mv data_bkg_barrelJet.dat    storeroot/data_bkg_barrelJet.dat
@@ -11,5 +16,6 @@ mv data_bkg_endcapJet.dat    storeroot/data_bkg_endcapJet.dat
 mv data_yield_allphoton.dat  storeroot/data_yield_allphoton.dat
 mv data_yield_barrelJet.dat  storeroot/data_yield_barrelJet.dat
 mv data_yield_endcapJet.dat  storeroot/data_yield_endcapJet.dat
+mv plots storefig/
 
 /bin/rm log*
