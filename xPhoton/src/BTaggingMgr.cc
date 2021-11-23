e86463ce42ee841ebaf6cb57211a031f3d933a2c
ltsai323 <johnson20050@gmail.com>
2021-11-23 14:31:28 +0800

    BTagCalibration : update to use different data era

--- xPhoton/src/BTaggingMgr.cc
+++ xPhoton/src/BTaggingMgr.cc
@@ -5,16 +5,16 @@
 #include <string>
 
 
-namespace t
+namespace
 {
     // use std::string due to a equal sign is needed. first : algorithm, second : systType
     std::map<std::string, std::vector<std::string> > systTypeCollection;
-    std::vector<const char*> flavourCollection;
+    //std::vector<const char*> flavourCollection;
     void SetContent(){
         if ( systTypeCollection.size() != 0 )  return;
-        flavourCollection.push_back("Bquark");
-        flavourCollection.push_back("Cquark");
-        flavourCollection.push_back("Lights");
+        //flavourCollection.push_back("Bquark");
+        //flavourCollection.push_back("Cquark");
+        //flavourCollection.push_back("Lights");
 
         systTypeCollection["CSVv2"] = {
             "central",
@@ -271,92 +271,45 @@ namespace t
             */
         };
     }
-}; // end of namespace t
-BTaggingMgr::BTaggingMgr() :
-
-    _usedAlgorithmNames(),
-    _systTypeRegisted(false)
-{
-}
-
-void BTaggingMgr::UseAlgorithm(std::string algorithmName)
-{
-    t::SetContent();
-    if ( t::systTypeCollection.find(algorithmName) == t::systTypeCollection.end() )
-        LOG_FATAL("Input name '%s' cannot be recognized in BTaggingMgr", algorithmName.c_str());
-    _usedAlgorithmNames.push_back(algorithmName);
-}
-
-void BTaggingMgr::RegisterSystTypes()
-{
-    if ( _systTypeRegisted )
-        LOG_FATAL("This function cannot be used twice. Please use it after all algorithms assigned");
-
-    _systTypeRegisted=true;
-    for ( auto usedAlgorithm : _usedAlgorithmNames )
-        _usedSystTypes[usedAlgorithm] = t::systTypeCollection[usedAlgorithm];
-
-    calibPTRs.clear(); calibReaderPTRs.clear();
-    calibPTRs.reserve(_usedSystTypes.size());
-    calibReaderPTRs.reserve(_usedSystTypes.size());
-
-    for ( auto usedAlgorithm : _usedAlgorithmNames )
+    std::vector<std::string> systTypes( std::string name )
     {
-        calibPTRs.push_back(
-                std::make_shared<BTagCalibration>( usedAlgorithm, ExternalFilesMgr::csvFile_BTagCalibs(usedAlgorithm) )
-                );
-        calibReaderPTRs.push_back(
-                std::make_shared<BTagCalibrationReader>( BTagEntry::OP_RESHAPING, "central", _usedSystTypes[usedAlgorithm] )
-                //std::make_shared<BTagCalibrationReader>( BTagEntry::OP_TIGHT, "central", _usedSystTypes[usedAlgorithm] )
-                );
-        BTagCalibration* calib = calibPTRs.back().get();
-
-        std::shared_ptr<BTagCalibrationReader> calibReaderPTR = calibReaderPTRs.back();
-        calibReaderPTR->load(*calib, BTagEntry::FLAV_B   , "iterativefit" );
-        calibReaderPTR->load(*calib, BTagEntry::FLAV_C   , "iterativefit" );
-        calibReaderPTR->load(*calib, BTagEntry::FLAV_UDSG, "iterativefit" );
-
-        /*
-        calibReaderPTR->load(*calib, BTagEntry::FLAV_B   , "comb" );
-        calibReaderPTR->load(*calib, BTagEntry::FLAV_C   , "comb" );
-        calibReaderPTR->load(*calib, BTagEntry::FLAV_UDSG, "comb" );
-        */
+        if ( systTypeCollection.size() == 0 ) SetContent();
+        return systTypeCollection[name];
     }
-    InitVars();
-}
-void BTaggingMgr::InitVars()
-{
-    if ( !_systTypeRegisted )
-        LOG_FATAL("You need to execute function RegisterSystTypes() before initialize variables.");
+}; // end of namespace t
+BTaggingMgr::BTaggingMgr( const char* dataera, const char* algoname, std::vector<std::string> systypenames ) :
+    algorithm(algoname),
+    _dataEra(dataera),
+    _usedSystTypeNames(systypenames),
+    _systVars(_usedSystTypeNames.size()),
+    _calib ( algorithm, ExternalFilesMgr::csvFile_BTagCalibs(algorithm, dataera) ), // era input asdf
+    _calibReader ( BTagEntry::OP_RESHAPING, "central", _usedSystTypeNames )
 
-    if ( systVars.size() == 0 ) // create new variables
-        for ( unsigned iAlgo = 0; iAlgo < _usedAlgorithmNames.size(); ++iAlgo )
-        {
-            const std::string algoName = _usedAlgorithmNames[iAlgo];
-            for ( unsigned iSyst = 0; iSyst < _usedSystTypes[algoName].size(); ++iSyst )
-            { systVars[ systVarIdx(iAlgo,iSyst) ] = 0; }
-        }
-    else
-        for ( auto systVarIter = systVars.begin(); systVarIter != systVars.end(); ++systVarIter )
-            systVarIter->second = 0;
+{
+    _calibReader.load(_calib, BTagEntry::FLAV_B   , "iterativefit" );
+    _calibReader.load(_calib, BTagEntry::FLAV_C   , "iterativefit" );
+    _calibReader.load(_calib, BTagEntry::FLAV_UDSG, "iterativefit" );
+    /*
+    _calibReader.load(*calib, BTagEntry::FLAV_B   , "comb" );
+    _calibReader.load(*calib, BTagEntry::FLAV_C   , "comb" );
+    _calibReader.load(*calib, BTagEntry::FLAV_UDSG, "comb" );
+    */
 }
+
+
+void BTaggingMgr::InitVars()
+{ std::fill( _systVars.begin(), _systVars.end(), 0 ); }
 void BTaggingMgr::RegBranch(TTree* t)
 {
-    if ( !_systTypeRegisted )
-        LOG_FATAL("You need to execute function RegisterSystTypes() before register branch into tree.");
     char bname[200], bnameF[200];
     if ( t == nullptr || t->IsZombie() ) LOG_FATAL("input tree is invalid");
 
-    for ( unsigned iAlgo = 0; iAlgo < _usedAlgorithmNames.size(); ++iAlgo )
+    for ( unsigned iSyst = 0; iSyst < _usedSystTypeNames.size(); ++iSyst )
     {
-        const std::string algorithm = _usedAlgorithmNames[iAlgo];
-        for ( unsigned iSyst = 0; iSyst < _usedSystTypes[algorithm].size(); ++iSyst )
-        {
-            const std::string systType = _usedSystTypes[algorithm][iSyst];
-            sprintf(bname , "jetSF.%s.%s"  , algorithm.c_str(), systType.c_str());
-            sprintf(bnameF, "jetSF.%s.%s/F", algorithm.c_str(), systType.c_str());
-            t->Branch(bname, &systVars[ systVarIdx(iAlgo,iSyst) ], bnameF);
-        }
+        const std::string systType = _usedSystTypeNames[iSyst];
+        sprintf(bname , "jetSF.%s.%s"  , algorithm, systType.c_str());
+        sprintf(bnameF, "jetSF.%s.%s/F", algorithm, systType.c_str());
+        t->Branch(bname, &_systVars[iSyst], bnameF);
     }
 }
 void BTaggingMgr::FillWeightToEvt(float pt_, float eta_, int hadFlav_, float bDis_)
@@ -364,26 +317,30 @@ void BTaggingMgr::FillWeightToEvt(float pt_, float eta_, int hadFlav_, float bDi
     float bDis = bDis_;
     if ( bDis_ < 0.0 ) bDis = -0.05;
     else if ( bDis_ > 1.0 ) bDis = 1.0;
-    for ( unsigned iAlgo = 0; iAlgo < _usedAlgorithmNames.size(); ++iAlgo )
+    for ( unsigned iSyst = 0; iSyst < _usedSystTypeNames.size(); ++iSyst )
     {
-        const std::string algorithm = _usedAlgorithmNames[iAlgo];
-        for ( unsigned iSyst = 0; iSyst < _usedSystTypes[algorithm].size(); ++iSyst )
+        if ( fabs(eta_) > 2.5 ) LOG_FATAL(" eta is out of range!!!");
+        if      ( hadFlav_ == HADRONFLAV_L )
+            _systVars[iSyst] = _calibReader.eval_auto_bounds( _usedSystTypeNames[iSyst], BTagEntry::FLAV_UDSG,eta_,pt_, bDis);
+        else if ( hadFlav_ == HADRONFLAV_C )
         {
-            if ( fabs(eta_) > 2.5 ) LOG_FATAL(" eta is out of range!!!");
-            if      ( hadFlav_ == HADRONFLAV_L )
-                systVars[ systVarIdx(iAlgo,iSyst) ] = calibReaderPTRs[iAlgo]->eval_auto_bounds( _usedSystTypes[algorithm][iSyst], BTagEntry::FLAV_UDSG,eta_,pt_, bDis);
-            else if ( hadFlav_ == HADRONFLAV_C )
-            {
-                systVars[ systVarIdx(iAlgo,iSyst) ] = calibReaderPTRs[iAlgo]->eval_auto_bounds( _usedSystTypes[algorithm][iSyst], BTagEntry::FLAV_C   ,eta_,pt_, bDis);
-                LOG_INFO(" C flavor gets weight : %.4f", systVars[ systVarIdx(iAlgo,iSyst) ] );
-            }
-            else if ( hadFlav_ == HADRONFLAV_B )
-                systVars[ systVarIdx(iAlgo,iSyst) ] = calibReaderPTRs[iAlgo]->eval_auto_bounds( _usedSystTypes[algorithm][iSyst], BTagEntry::FLAV_B   ,eta_,pt_, bDis);
-            else
-                LOG_WARNING( "none of known hadron flavour input. Give a -999 for check" );
-
+            _systVars[iSyst] = _calibReader.eval_auto_bounds( _usedSystTypeNames[iSyst], BTagEntry::FLAV_C   ,eta_,pt_, bDis);
+            LOG_INFO(" C flavor gets weight : %.4f", _systVars[iSyst] );
         }
+        else if ( hadFlav_ == HADRONFLAV_B )
+            _systVars[iSyst] = _calibReader.eval_auto_bounds( _usedSystTypeNames[iSyst], BTagEntry::FLAV_B   ,eta_,pt_, bDis);
+        else
+            LOG_WARNING( "none of known hadron flavour input. Give a -999 for check" );
     }
 }
 
 
+BTaggingMgr_CSVv2      ::BTaggingMgr_CSVv2      ( const char* dataera ) :
+    BTaggingMgr(dataera, "CSVv2"      , ::systTypes("CSVv2"      ))
+{}
+BTaggingMgr_DeepCSV    ::BTaggingMgr_DeepCSV    ( const char* dataera ) :
+    BTaggingMgr(dataera, "DeepCSV"    , ::systTypes("DeepCSV"    ))
+{}
+BTaggingMgr_DeepFlavour::BTaggingMgr_DeepFlavour( const char* dataera ) :
+    BTaggingMgr(dataera, "DeepFlavour", ::systTypes("DeepFlavour"))
+{}
