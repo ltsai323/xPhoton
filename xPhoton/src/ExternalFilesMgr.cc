bd3fcf66a36097af6c944096cf344006abb7a277
ltsai323 <johnson20050@gmail.com>
2022-02-24 16:39:32 +0800

    ExternalFileMgr : new UL2018 BDT mva trained.

--- xPhoton/src/ExternalFilesMgr.cc
+++ xPhoton/src/ExternalFilesMgr.cc
@@ -193,7 +193,7 @@ const char* ExternalFilesMgr::xmlFile_MVAweight(int isEndcap, std::string era)
         else if ( era == "UL2017"     )
             return "";
         else if ( era == "UL2018"     )
-            return "/wk_cms/ltsai/ReceivedFile/RSprocessedFiles/xmlfiles/external/autumn18_EE_TMVAnalysis_BDT.weights.xml";
+            return "/home/ltsai/ReceivedFile/GJet/TMVATrainingResult/UL2018/BDT_EE/dataset/weights/TMVAnalysis_BDT.weights.xml";
     }
     else
     {
@@ -208,7 +208,7 @@ const char* ExternalFilesMgr::xmlFile_MVAweight(int isEndcap, std::string era)
         else if ( era == "UL2017"     )
             return "";
         else if ( era == "UL2018"     )
-            return "/wk_cms/ltsai/ReceivedFile/RSprocessedFiles/xmlfiles/external/autumn18_EB_TMVAnalysis_BDT.weights.xml";
+            return "/home/ltsai/ReceivedFile/GJet/TMVATrainingResult/UL2018/BDT_EB/dataset/weights/TMVAnalysis_BDT.weights.xml";
     }
     return "";
 }
