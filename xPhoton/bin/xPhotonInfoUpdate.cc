7c430c8cc7a453edbb1db786d3f15484f2d1253f
ltsai323 <johnson20050@gmail.com>
2022-04-25 15:36:36 +0800

    xPhotonInfoUpdate : finished to separate mvaUpdate

--- xPhoton/bin/xPhotonInfoUpdate.cc
+++ xPhoton/bin/xPhotonInfoUpdate.cc
@@ -67,22 +67,89 @@ struct JsonInfo
     std::vector<std::string> inputfiles;
 };
 
-//void UpdateMVA( const JsonInfo& jobInfo, TTree* inTree )
-int main(int argc, char** argv)
+class UpdateMVAProcedure
 {
-    JsonInfo inputvars(argc,argv);
+    public:
+    UpdateMVAProcedure( const JsonInfo* jobInfo_, ShowerShapeCorrectionAdapter* SScorr_, TTree* outtree ) : jobInfo(jobInfo_), SScorr(SScorr_)
+    {
+        if (!jobInfo->updateMVA ) return;
+        for ( int iBE = 0; iBE < 2; ++iBE )
+        {
+            tmvaReader[iBE] = new TMVA::Reader("!Color:Silent");
 
-    TChain* ch = new TChain("t");
-    for ( const std::string& ifile : inputvars.inputfiles )
-        ch->Add(ifile.c_str());
-    // init of main
-    
-    TTree* inTree = ch;
-    const JsonInfo& jobInfo = inputvars;
-    inTree->SetBranchStatus("*", 1);
-    TreeReader data(inTree);
+            if ( jobInfo->dataEra == "UL2018" )
+            {
+                tmvaReader[iBE]->AddVariable("r9Full5x5", &r9Full5x5);
+                tmvaReader[iBE]->AddVariable( "sieieFull5x5", &sieieFull5x5 );
+                tmvaReader[iBE]->AddVariable( "sieipFull5x5", &sieipFull5x5 );     
+                tmvaReader[iBE]->AddVariable( "s4Full5x5", &s4Full5x5 );	       
+                tmvaReader[iBE]->AddVariable("rawE", &rawE );
+                tmvaReader[iBE]->AddVariable("scEtaWidth", &scEtaWidth );
+                tmvaReader[iBE]->AddVariable("scPhiWidth", &scPhiWidth );
+                if (iBE == 1)
+                {
+                tmvaReader[iBE]->AddVariable("esEnergyOverSCRawEnergy", &esEnergyOverSCRawEnergy );
+                tmvaReader[iBE]->AddVariable("esRR", &esRR );
+                }
+                tmvaReader[iBE]->AddVariable("recoPhi", &recoPhi);
+                tmvaReader[iBE]->AddVariable("recoSCEta", &recoSCEta );
+                tmvaReader[iBE]->AddVariable("rho", &rho);
 
-    // declare MVA {{{
+
+                std::cout << "PhotonMVAcalculator : using " << ExternalFilesMgr::xmlFile_MVAweight(iBE, jobInfo->dataEra) << std::endl;
+                tmvaReader[iBE]->BookMVA("BDT", ExternalFilesMgr::xmlFile_MVAweight(iBE, jobInfo->dataEra) );
+            }
+            else { throw "Error ! input dataera is not defined. So variable order is needed to be modified\n"; }
+        }
+        outtree->SetBranchStatus("mva", 0);
+        if ( outtree->GetListOfBranches()->FindObject("calib_mva") )
+            outtree->SetBranchStatus("calib_mva", 0);
+        outtree->Branch("mva", &mva, "mva/F");
+        outtree->Branch("calib_mva", &calib_mva, "calib_mva/F");
+    }
+
+    void UpdateValue(TreeReader* data)
+    {
+        if (!jobInfo->updateMVA ) return;
+        bool isEE = fabs(data->GetFloat("recoSCEta")) > 1.5 ? 1 : 0;
+
+        if ( jobInfo->isMC )
+        {
+            ShowerShapeCorrectionParameters_xPhoton::loadVars(SScorr, data);
+            SScorr->CalculateCorrections();
+        }
+
+        // TMVA used pars.
+        recoPhi                  = data->GetFloat("recoPhi"                );
+        r9Full5x5                = data->GetFloat("r9Full5x5"              );
+        sieieFull5x5             = data->GetFloat("sieieFull5x5"           );
+        sieipFull5x5             = data->GetFloat("sieipFull5x5"           );
+        s4Full5x5                = data->GetFloat("s4Full5x5"              );
+        recoSCEta                = data->GetFloat("recoSCEta"              );
+        rawE                     = data->GetFloat("rawE"                   );
+        scEtaWidth               = data->GetFloat("scEtaWidth"             );
+        scPhiWidth               = data->GetFloat("scPhiWidth"             );
+
+        esEnergyOverSCRawEnergy  = data->GetFloat("esEnergyOverSCRawEnergy");
+        esRR                     = data->GetFloat("esRR"                   );
+        rho                      = data->GetFloat("rho"                    );
+
+        
+        mva = tmvaReader[isEE]->EvaluateMVA("BDT");
+        if ( jobInfo->isMC )
+        {
+            r9Full5x5               = SScorr->Corrected( ShowerShapeCorrectionAdapter::r9       );
+            s4Full5x5               = SScorr->Corrected( ShowerShapeCorrectionAdapter::s4       );
+            sieieFull5x5            = SScorr->Corrected( ShowerShapeCorrectionAdapter::sieie    );
+            sieipFull5x5            = SScorr->Corrected( ShowerShapeCorrectionAdapter::sieip    );
+            scEtaWidth   	        = SScorr->Corrected( ShowerShapeCorrectionAdapter::etaWidth );
+            scPhiWidth   	        = SScorr->Corrected( ShowerShapeCorrectionAdapter::etaWidth );
+            esEnergyOverSCRawEnergy = SScorr->Corrected( ShowerShapeCorrectionAdapter::esEnergyOverSCRawEnergy);
+        }
+        calib_mva = tmvaReader[isEE]->EvaluateMVA("BDT");
+    }
+    const JsonInfo* jobInfo;
+    ShowerShapeCorrectionAdapter* SScorr;
     float 
         recoPhi,
         r9Full5x5,
@@ -98,57 +165,30 @@ int main(int argc, char** argv)
         esRR,
         rho;
     TMVA::Reader* tmvaReader[2];
-    for ( int iBE = 0; iBE < 2; ++iBE )
-    {
-        tmvaReader[iBE] = new TMVA::Reader("!Color:Silent");
+    float mva;
+    float calib_mva;
+};
+int main(int argc, char** argv)
+{
+    JsonInfo inputvars(argc,argv);
 
-        // add classification variables
-        tmvaReader[iBE]->AddVariable("recoPhi", &recoPhi);
-        tmvaReader[iBE]->AddVariable("r9Full5x5", &r9Full5x5);
-        tmvaReader[iBE]->AddVariable( "sieieFull5x5", &sieieFull5x5 );
-        tmvaReader[iBE]->AddVariable( "sieipFull5x5", &sieipFull5x5 );     
-        if ( jobInfo.dataEra == "2016ReReco" )
-        {
-            //tmvaReader[iBE]->AddVariable("r9", &r9Full5x5);
-            tmvaReader[iBE]->AddVariable( "s4 := e2x2Full5x5/e5x5Full5x5", &s4Full5x5 );	       
-        }
-        if ( jobInfo.dataEra == "UL2018" )
-        {
-            tmvaReader[iBE]->AddVariable( "s4Full5x5", &s4Full5x5 );	       
-        }
-        tmvaReader[iBE]->AddVariable("recoSCEta", &recoSCEta );
-        tmvaReader[iBE]->AddVariable("rawE", &rawE );
-        tmvaReader[iBE]->AddVariable("scEtaWidth", &scEtaWidth );
-        tmvaReader[iBE]->AddVariable("scPhiWidth", &scPhiWidth );
-        if (iBE == 1)
-        {
-            if ( jobInfo.dataEra == "2016ReReco" )
-                tmvaReader[iBE]->AddVariable("ESEn := esEn/rawE", &esEnergyOverSCRawEnergy );
-            if ( jobInfo.dataEra == "UL2018" )
-                tmvaReader[iBE]->AddVariable("esEnergyOverSCRawEnergy", &esEnergyOverSCRawEnergy );
-            tmvaReader[iBE]->AddVariable("esRR", &esRR );
-        }
-        tmvaReader[iBE]->AddVariable("rho", &rho);
+    TChain* ch = new TChain("t");
+    for ( const std::string& ifile : inputvars.inputfiles )
+        ch->Add(ifile.c_str());
+    // init of main
+    
+    ch->SetBranchStatus("*", 1);
 
-
-        std::cout << "PhotonMVAcalculator : using " << ExternalFilesMgr::xmlFile_MVAweight(iBE, jobInfo.dataEra) << std::endl;
-        tmvaReader[iBE]->BookMVA("BDT", ExternalFilesMgr::xmlFile_MVAweight(iBE, jobInfo.dataEra) );
-    }
-    // declare MVA done }}}
-    ShowerShapeCorrectionAdapter SScorr( jobInfo.dataEra, jobInfo.isMC );
-
-
-    TFile* outfile = new TFile( jobInfo.outputfilename.c_str(), "RECREATE" );
+    TFile* outfile = new TFile( inputvars.outputfilename.c_str(), "RECREATE" );
     outfile->cd();
-    TTree* outtree = inTree->CloneTree(0);
+    TTree* outtree = ch->CloneTree(0);
     outtree->SetAutoSave(ANNOUNSE_INTERVAL);
 
-    float mva = 0;
-    float calib_mva = 0;
-    outtree->SetBranchStatus("mva", 0);
-    outtree->SetBranchStatus("calib_mva", 0);
-    outtree->Branch("mva", &mva, "mva/F");
-    outtree->Branch("calib_mva", &calib_mva, "calib_mva/F");
+    TreeReader data(ch);
+    ShowerShapeCorrectionAdapter SScorr( inputvars.dataEra, inputvars.isMC );
+
+    UpdateMVAProcedure updatingmvascore(&inputvars, &SScorr, outtree);
+
 
     LOG_INFO(" processing entries %lld \n", data.GetEntriesFast());
     for ( Long64_t evtIdx = 0; evtIdx != data.GetEntriesFast(); ++evtIdx )
@@ -156,46 +196,9 @@ int main(int argc, char** argv)
         
         if ( evtIdx % ANNOUNSE_INTERVAL == 0 ) LOG_INFO(" processing enty %llu / %llu = %.2f %% \n", evtIdx, data.GetEntriesFast(), float(10000*evtIdx/data.GetEntriesFast())/100.);
         
-        inTree->GetEntry(evtIdx);
+        ch->GetEntry(evtIdx);
         data.GetEntry(evtIdx);
-        // variables declare and data loader {{{
-        bool isEE = fabs(data.GetFloat("recoSCEta")) > 1.5 ? 1 : 0;
-
-        if ( jobInfo.isMC )
-        {
-            ShowerShapeCorrectionParameters_xPhoton::loadVars(&SScorr, &data);
-            SScorr.CalculateCorrections();
-        }
-
-        // TMVA used pars.
-        recoPhi                  = data.GetFloat("recoPhi"                );
-        r9Full5x5                = data.GetFloat("r9Full5x5"              );
-        sieieFull5x5             = data.GetFloat("sieieFull5x5"           );
-        sieipFull5x5             = data.GetFloat("sieipFull5x5"           );
-        s4Full5x5                = data.GetFloat("s4Full5x5"              );
-        recoSCEta                = data.GetFloat("recoSCEta"              );
-        rawE                     = data.GetFloat("rawE"                   );
-        scEtaWidth               = data.GetFloat("scEtaWidth"             );
-        scPhiWidth               = data.GetFloat("scPhiWidth"             );
-
-        esEnergyOverSCRawEnergy  = data.GetFloat("esEnergyOverSCRawEnergy");
-        esRR                     = data.GetFloat("esRR"                   );
-        rho                      = data.GetFloat("rho"                    );
-
-        // variables declare and data loader end }}}
-        
-        mva = tmvaReader[isEE]->EvaluateMVA("BDT");
-        if ( jobInfo.isMC )
-        {
-            r9Full5x5               = SScorr.Corrected( ShowerShapeCorrectionAdapter::r9       );
-            s4Full5x5               = SScorr.Corrected( ShowerShapeCorrectionAdapter::s4       );
-            sieieFull5x5            = SScorr.Corrected( ShowerShapeCorrectionAdapter::sieie    );
-            sieipFull5x5            = SScorr.Corrected( ShowerShapeCorrectionAdapter::sieip    );
-            scEtaWidth   	        = SScorr.Corrected( ShowerShapeCorrectionAdapter::etaWidth );
-            scPhiWidth   	        = SScorr.Corrected( ShowerShapeCorrectionAdapter::etaWidth );
-            esEnergyOverSCRawEnergy = SScorr.Corrected( ShowerShapeCorrectionAdapter::esEnergyOverSCRawEnergy);
-        }
-        calib_mva = tmvaReader[isEE]->EvaluateMVA("BDT");
+        updatingmvascore.UpdateValue(&data);
 
         outtree->Fill();
     }
