45f886689e77215fcfe0fda31b18ae13f7ba7486
ltsai323 <johnson20050@gmail.com>
2022-04-29 09:44:26 +0800

    one click script is finished

--- macros/fullrun.step2.sh
+++ macros/fullrun.step2.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env sh
-datafile=/home/ltsai/ReceivedFile/GJet/latestsample/UL2018/correctMVA/data.root
-fakefile=/home/ltsai/ReceivedFile/GJet/latestsample/UL2018/correctMVA/qcdpythia.root
-signfile=/home/ltsai/ReceivedFile/GJet/latestsample/UL2018/correctMVA/sigpythia.root
+if [ "$1" == "" ]; then
+    echo 'use files listed internally'
+    datafile=/home/ltsai/ReceivedFile/GJet/latestsample/UL2018/correctMVA/data.root
+    fakefile=/home/ltsai/ReceivedFile/GJet/latestsample/UL2018/correctMVA/qcdpythia.root
+    signfile=/home/ltsai/ReceivedFile/GJet/latestsample/UL2018/correctMVA/sigpythia.root
+else
+    echo 'use files externally'
+    source $1
+fi
+echo "file list:"
+echo "data : $datafile"
+echo "sign : $signfile"
+echo "fake : $fakefile"
+sleep 2
+
 
 cd step2.makehistos
 sh run.sh $datafile $signfile $fakefile
