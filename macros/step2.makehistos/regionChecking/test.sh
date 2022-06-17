3ff24ca3004c3e49ab1c7ec0392ce131ee0d7ae1
ltsai323 <johnson20050@gmail.com>
2022-06-17 16:45:01 +0800

    add checking region for test

---
+++ macros/step2.makehistos/regionChecking/test.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+datafile=new2016.data.root
+signfile=new2016.sign.root
+fakefile=
+
+echo "used data file: $datafile"
+echo "used sign file: $signfile"
+echo "used fake file: $fakefile"
+
+sleep 5
+
+label=data
+ifile=$datafile
+isMC=false
+root -b <<EOF
+.L MakeHisto.C
+MakeHisto t("$ifile","$label",$isMC, 0)
+t.Loop(1)
+EOF
+
+label=sig
+ifile=$signfile
+isMC=true
+root -b <<EOF
+.L MakeHisto.C
+MakeHisto t("$ifile","$label",$isMC, 0)
+t.Loop(1)
+EOF
+# 
+# 
+# label=QCD
+# ifile=$fakefile
+# isMC=true
+# 
+# root -b <<EOF
+# .L MakeHisto.C+
+# MakeHisto t("$ifile","$label",$isMC, 1)
+# t.Loop(1)
+# EOF
