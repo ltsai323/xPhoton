be27640fa2ba780d6c58417cbc383a7a52e7e8e4
ltsai323 <johnson20050@gmail.com>
2023-01-18 12:49:40 +0800

    accomplish quarkCompositionCheck

--- analysis/scripts/quickcheck.quarkCompositionCheck.signalANDsideband/qcdCHECKER_plotQuarkCompositionSep_differentSelection.C
+++ analysis/scripts/quickcheck.quarkCompositionCheck.signalANDsideband/qcdCHECKER_plotQuarkCompositionSep_differentSelection.C
@@ -40,6 +40,7 @@ TH1* histFactory_jetPt_TreeSelection(TDirectoryFile* ifile, const char* cut)
 {
     TTree* t = (TTree*) ifile->Get("t");
     TH1F* outhist = new TH1F( "tmp", "", jetptBin.size()-1, &(jetptBin.front()) );
+    outhist->Sumw2();
     t->Draw("jetPt>>tmp", cut);
     return outhist;
 }
@@ -67,8 +68,49 @@ void normalization_eachBinBackToOne(std::vector<TH1*>& hists)
             hist->SetBinContent(ibin, hist->GetBinContent(ibin)/integral);
     }
 }
+std::vector<TH1*> normalizeToOne(const std::vector<TH1*>& hists)
+{
+    std::vector<TH1*> normalizedhists(hists.size(), nullptr);
+    for ( int idx = 0; idx < hists.size(); ++idx )
+        normalizedhists[idx] = (TH1*) hists[idx]->Clone();
+    normalizedhists[quarks::b]->SetName("bComposition");
+    normalizedhists[quarks::c]->SetName("cComposition");
+    normalizedhists[quarks::L]->SetName("LComposition");
 
 
+    TH1* h = hists.front();
+    for ( auto ibin = h->GetNbinsX(); ibin != 0; --ibin )
+    {
+        double integral = 0.;
+        for ( TH1* hist : hists )
+            integral += hist->GetBinContent(ibin);
+        for ( int idx = 0; idx < hists.size(); ++idx )
+            normalizedhists[idx]->SetBinContent(ibin, hists[idx]->GetBinContent(ibin)/integral);
+    }
+    return normalizedhists;
+}
+
+
+std::vector<TH1*> GetQuarkYields(TDirectoryFile* ifile, const std::string& selection_, const std::string& weight_)
+{
+    const char* selection = selection_.c_str();
+    const char* weight = weight_.c_str();
+    std::vector<TH1*> quark_hists(quarks::totNum,nullptr);
+    // load b
+    quark_hists[quarks::b] =
+        histFactory_jetPt_TreeSelection(ifile, Form("(%s&&%s) * %s",selection, "jetHadFlvr==5", weight) );
+    SetHistProperty(quark_hists[quarks::b], colors[0], "bYield", "b quark");
+    // load c
+    quark_hists[quarks::c] =
+        histFactory_jetPt_TreeSelection(ifile, Form("(%s&&%s) * %s",selection, "jetHadFlvr==4", weight) );
+    SetHistProperty(quark_hists[quarks::c], colors[1], "cYield", "c quark");
+    // load L
+    quark_hists[quarks::L] =
+        histFactory_jetPt_TreeSelection(ifile, Form("(%s&&%s) * %s",selection, "jetHadFlvr==0", weight) );
+    SetHistProperty(quark_hists[quarks::L], colors[2], "LYield", "light quark");
+
+    return quark_hists;
+}
 
 std::vector<TH1*> GetQuarkCompositions(TDirectoryFile* ifile, const std::string& selection_, const std::string& weight_)
 {
@@ -127,33 +169,61 @@ void qcdCHECKER_plotQuarkCompositionSep_differentSelection()
     std::string weight;
     std::string basicCuts = "(fabs(recoSCEta)<1.4442||(fabs(recoSCEta)<2.5&&fabs(recoSCEta)>1.566) && MET/recoPt<0.7 && eleVeto!=0 && ( (fabs(recoSCEta)<1.5&&sieieFull5x5<0.012&&HoverE<0.08) || (fabs(recoSCEta)>1.5&&sieieFull5x5<0.027&&HoverE<0.05) )";
 
-    tag = "rawSign";
-    cut = "fabs(chIsoRaw)<2";
-    weight="puwei*mcweight";
-    recordedHists.push_back( std::make_pair(tag.c_str(),GetQuarkCompositions(ifile,cut,weight)) );
-    for ( TH1* hist : recordedHists.back().second )
-    { hist->SetName( Form("%s_%s", tag.c_str(), hist->GetName()) ); hist->Write(); }
+    {
+        tag = "rawSign";
+        cut = "fabs(chIsoRaw)<2";
+        weight="puwei*mcweight";
+        //recordedHists.push_back( std::make_pair(tag.c_str(),GetQuarkCompositions(ifile,cut,weight)) );
+        std::vector<TH1*> yields = GetQuarkYields(ifile,cut,weight);
+        std::vector<TH1*> compositions = normalizeToOne(yields);
+        yields.insert(yields.end(), compositions.begin(), compositions.end());
 
-    tag = "rawFake";
-    cut = "fabs(chIsoRaw)>6 && fabs(chIsoRaw)<13";
-    weight="puwei*mcweight";
-    recordedHists.push_back( std::make_pair(tag.c_str(),GetQuarkCompositions(ifile,cut,weight)) );
-    for ( TH1* hist : recordedHists.back().second )
-    { hist->SetName( Form("%s_%s", tag.c_str(), hist->GetName()) ); hist->Write(); }
+        recordedHists.push_back( std::make_pair(tag.c_str(),yields) );
+        for ( TH1* hist : recordedHists.back().second )
+        { hist->SetName( Form("%s_%s", tag.c_str(), hist->GetName()) ); hist->Write(); }
+    }
 
-    tag = "cutSign";
-    cut = "passMaxPUcut && fabs(chIsoRaw)<2";
-    weight="weight_passMaxPUcut*puwei*mcweight";
-    recordedHists.push_back( std::make_pair(tag.c_str(),GetQuarkCompositions(ifile,cut,weight)) );
-    for ( TH1* hist : recordedHists.back().second )
-    { hist->SetName( Form("%s_%s", tag.c_str(), hist->GetName()) ); hist->Write(); }
+    {
+        tag = "rawFake";
+        cut = "fabs(chIsoRaw)>6 && fabs(chIsoRaw)<13";
+        weight="puwei*mcweight";
+        //recordedHists.push_back( std::make_pair(tag.c_str(),GetQuarkCompositions(ifile,cut,weight)) );
+        std::vector<TH1*> yields = GetQuarkYields(ifile,cut,weight);
+        std::vector<TH1*> compositions = normalizeToOne(yields);
+        yields.insert(yields.end(), compositions.begin(), compositions.end());
 
-    tag = "cutFake";
-    cut = "passMaxPUcut && fabs(chIsoRaw)>6 && fabs(chIsoRaw)<13";
-    weight="weight_passMaxPUcut*puwei*mcweight";
-    recordedHists.push_back( std::make_pair(tag.c_str(),GetQuarkCompositions(ifile,cut,weight)) );
-    for ( TH1* hist : recordedHists.back().second )
-    { hist->SetName( Form("%s_%s", tag.c_str(), hist->GetName()) ); hist->Write(); }
+        recordedHists.push_back( std::make_pair(tag.c_str(),yields) );
+        for ( TH1* hist : recordedHists.back().second )
+        { hist->SetName( Form("%s_%s", tag.c_str(), hist->GetName()) ); hist->Write(); }
+    }
+
+    {
+        tag = "cutSign";
+        cut = "passMaxPUcut && fabs(chIsoRaw)<2";
+        weight="weight_passMaxPUcut*puwei*mcweight";
+        //recordedHists.push_back( std::make_pair(tag.c_str(),GetQuarkCompositions(ifile,cut,weight)) );
+        std::vector<TH1*> yields = GetQuarkYields(ifile,cut,weight);
+        std::vector<TH1*> compositions = normalizeToOne(yields);
+        yields.insert(yields.end(), compositions.begin(), compositions.end());
+
+        recordedHists.push_back( std::make_pair(tag.c_str(),yields) );
+        for ( TH1* hist : recordedHists.back().second )
+        { hist->SetName( Form("%s_%s", tag.c_str(), hist->GetName()) ); hist->Write(); }
+    }
+
+    {
+        tag = "cutFake";
+        cut = "passMaxPUcut && fabs(chIsoRaw)>6 && fabs(chIsoRaw)<13";
+        weight="weight_passMaxPUcut*puwei*mcweight";
+        //recordedHists.push_back( std::make_pair(tag.c_str(),GetQuarkCompositions(ifile,cut,weight)) );
+        std::vector<TH1*> yields = GetQuarkYields(ifile,cut,weight);
+        std::vector<TH1*> compositions = normalizeToOne(yields);
+        yields.insert(yields.end(), compositions.begin(), compositions.end());
+
+        recordedHists.push_back( std::make_pair(tag.c_str(),yields) );
+        for ( TH1* hist : recordedHists.back().second )
+        { hist->SetName( Form("%s_%s", tag.c_str(), hist->GetName()) ); hist->Write(); }
+    }
 
     ofile->Close();
     return;
