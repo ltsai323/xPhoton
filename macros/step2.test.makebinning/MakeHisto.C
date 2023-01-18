c05da0c6df17a32fcf09cc5c9e3dfeba135391be
ltsai323 <johnson20050@gmail.com>
2023-01-18 13:01:08 +0800

    test makehistos

--- macros/step2.test.makebinning/MakeHisto.C
+++ macros/step2.test.makebinning/MakeHisto.C
@@ -121,6 +121,7 @@ void MakeHisto::Loop(Int_t extracut = 0)
 
     Long64_t nbytes = 0, nb = 0;
     for (Long64_t jentry=0; jentry<nentries;jentry++)
+    //for (Long64_t jentry=0; jentry<10000;jentry++)
     {
         bool basicRecording = true;
         bool fiducialPhoton = true;
@@ -160,10 +161,9 @@ void MakeHisto::Loop(Int_t extracut = 0)
 
 	
 
-        float btag_evtweight_central     = eventweight * bTagWeight_Central(jetflvBin);
-        float btag_evtweight_up  = eventweight * bTagWeight_Up(jetflvBin);
-        float btag_evtweight_down= eventweight * bTagWeight_Down(jetflvBin);
-              btag_evtweight_up  = 1.;
+        float btag_evtweight_central    = eventweight * bTagWeight_Central(jetflvBin);
+        float btag_evtweight_up         = eventweight * bTagWeight_Up(jetflvBin);
+        float btag_evtweight_down       = eventweight * bTagWeight_Down(jetflvBin);
 
 
 
@@ -460,6 +460,7 @@ Int_t MakeHisto::triggerbit( const std::string& dataera, Int_t ptbin){
 }
 Int_t MakeHisto::JetFlavourBin( int jethadflvr )
 {
+    if (!IsMC() ) return 0;
     // 2 : b // 1 : c // 0 : light
     if     (fabs(jethadflvr)==5) return 2;
     else if(fabs(jethadflvr)==4) return 1;
