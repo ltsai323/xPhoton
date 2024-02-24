132db85241c91e40942e1e26b7cf00f569ff9304
ltsai323 <johnson20050@gmail.com>
2024-02-24 12:34:34 +0800

    input a yaml file for arguments

--- macros/step5.CompareYields/PhotonCrossSection.py
+++ macros/step5.CompareYields/PhotonCrossSection.py
@@ -3,8 +3,8 @@
 import ROOT
 from array import array
 
-from py_pt_ranges_definition import PhoPtBinning
-from py_pt_ranges_definition import FindEffLumi
+#from py_pt_ranges_definition import PhoPtBinning
+#from py_pt_ranges_definition import FindEffLumi
 from DATReadingTools import ReadEvt_FitResult, ReadEvt_Eff, BinValue
 
 
@@ -112,6 +112,44 @@ def MyLegend_Sep(hists) -> ROOT.TLegend:
 
 
 
+def PhotonCrossSection(pETAbin,jETAbin, ptBINNING, fitVALUES, effSOURCES:dict) -> list:
+    output = []
+    for pPtBin, ptLow in enumerate(ptBINNING):
+        if pPtBin == len(ptBINNING)-1: break
+        ptHigh = ptBINNING[pPtBin+1]
+
+        try:
+            fitvalue, fiterror = BinValue(pETAbin,jETAbin,pPtBin, fitVALUES)
+        except ValueError as e:
+            print(f'BinValue (pEtaBbin,jEtaBin,pPtBin) = ({pETAbin},{jETAbin},{pPtBin}) not found. Skip entry.')
+            continue
+        bin_numerator = fitvalue
+
+        eff_sum = 1.
+        for effname,effsource in effSOURCES.items():
+            jetabin = 0 if effname == 'presel' else jETAbin
+            effval,efferr = BinValue(pETAbin,jetabin,pPtBin, effsource)
+            eff_sum *= effval
+
+        fbTOpb = 1e-3 # 1fb = 0.001pb
+        eff_lumi = FindEffLumi(dataEra,pPtBin) / fbTOpb # fb-1 -> pb-1
+        peta_width=1.4442*2. if pETAbin == 0 else (2.5-1.566)*2.
+        jeta_width=1.5*2.    if jETAbin == 0 else (2.4-1.5  )*2.
+        ppt_width=ptHigh-ptLow
+        bin_denominator = peta_width*jeta_width*ppt_width * eff_lumi * eff_sum
+
+        output.append( {
+            'pEtaBin':pETAbin,
+            'jEtaBin':jetabin,
+            'pPtBin':pPtBin,
+            'pPtLow':ptLow,
+            'pPtHigh':ptHigh,
+            'crossSection': fitvalue / bin_denominator,
+            'crossSectionErrorUp': fiterror / bin_denominator, # asdf quote no efficiency
+            'crossSectionErrorDn': fiterror / bin_denominator,
+            } )
+    return output
+
 def Hist_PhotonCrossSection(pETAbin,jETAbin, ptBINNING, fitVALUES, effSOURCES:dict) -> ROOT.TH1F:
     histname = 'h_photonCrossSection_%d_%d' % (pETAbin,jETAbin)
     hist = ROOT.TH1F( histname, '#gamma+jet cross section', len(ptBINNING)-1, ptBINNING )
@@ -121,7 +159,11 @@ def Hist_PhotonCrossSection(pETAbin,jETAbin, ptBINNING, fitVALUES, effSOURCES:di
     for binIdx in range(1,hist.GetNbinsX()+1):
         pPtBin = binIdx-1
 
-        fitvalue, fiterror = BinValue(pETAbin,jETAbin,pPtBin, fitVALUES)
+        try:
+            fitvalue, fiterror = BinValue(pETAbin,jETAbin,pPtBin, fitVALUES)
+        except ValueError as e:
+            print(f'BinValue (pEtaBbin,jEtaBin,pPtBin) = ({pETAbin},{jETAbin},{pPtBin}) not found. Skip entry.')
+            continue
         bin_numerator = fitvalue
 
 
@@ -142,24 +184,34 @@ def Hist_PhotonCrossSection(pETAbin,jETAbin, ptBINNING, fitVALUES, effSOURCES:di
         hist.SetBinError(binIdx,0.) #no error currently
     return hist
 if __name__ == '__main__':
-    #inputfile='16yield_newformat.dat'
-    inputfile='/home/ltsai/ReceivedFile/GJet/latestsample/2016ReReco_ctagReshaped/deepcsv_CUT_NOCUT/2016ReReco.data.yield.dat'
-    fitvalues = ReadEvt_FitResult(inputfile)
+    import sys
+    import yaml
+    with open(sys.argv[1], 'r') as inCONFIG:
+        configs = yaml.safe_load(inCONFIG)
 
-    effsource = {}
-    jetselectfile = '/wk_cms/ltsai/github/xPhoton/macros/step2.1.efficienciesCalc/UL2016PreVFP/dat_jetSelections.dat'
-    signalregfile = '/wk_cms/ltsai/github/xPhoton/macros/step2.1.efficienciesCalc/UL2016PreVFP/dat_phoControlReg.dat'
-    phoselectfile = '/wk_cms/ltsai/github/xPhoton/macros/step2.1.efficienciesCalc/UL2016PreVFP/dat_preselectionEfficiency.dat'
+    fitvalues = ReadEvt_FitResult(configs['data'])
+    effsource = { key:ReadEvt_Eff(infile) for key,infile in configs['efficiencies'].items() }
+    def outfile(tag, extension):
+        lists = list( filter(None,[configs['outputtag'],tag]) )
+        outfile = '_'.join(lists) + extension
+        print(f'[File Generated] {outfile}')
+        return outfile
 
-    effsource['presel'] = ReadEvt_Eff(phoselectfile)
-    effsource['sigreg'] = ReadEvt_Eff(signalregfile)
-    effsource['jetsel'] = ReadEvt_Eff(jetselectfile)
 
-    pho_presel = 'newdat'
+    dataEra = configs['dataEra']
+    import importlib.util
+    def import_module_from_file():
+        file_path = configs['py_pt_definition']
+        spec = importlib.util.spec_from_file_location("pt_def", file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
+    pt_range_defitions = import_module_from_file()
+    PhoPtBinning = pt_range_defitions.PhoPtBinning
+    FindEffLumi = pt_range_defitions.FindEffLumi
 
-    dataEra = 'UL2016PreVFP'
-    pt_binning = array( 'd', PhoPtBinning(dataEra) )
-    for idx, val in enumerate(pt_binning): print('- bin %2d : %.0f'%(idx,val) )
+    pt_binning = array( 'd', PhoPtBinning(configs['dataEra']) )
+    #for idx, val in enumerate(pt_binning): print('- bin %2d : %.0f'%(idx,val) )
 
     canv = NewCanvas(False)
 
@@ -167,10 +219,12 @@ if __name__ == '__main__':
     markers = [34,33,28,27]
     #colors = [2,45,8,29]
     histIDX = 0
+    value_output = []
     for pEtaBin in range(2):
         for jEtaBin in range(2):
             canv.cd(1)
             hist = Hist_PhotonCrossSection(pEtaBin,jEtaBin, pt_binning, fitvalues, effsource)
+            value_output.extend(PhotonCrossSection(pEtaBin,jEtaBin, pt_binning, fitvalues, effsource))
 
             hist.SetLineWidth(2)
             hist.SetMarkerStyle(markers[histIDX])
@@ -185,7 +239,7 @@ if __name__ == '__main__':
         hist.Draw('EP SAME')
     leg = MyLegend(hists)
     leg.Draw()
-    canv.SaveAs('h_photonCrossSection.pdf')
+    canv.SaveAs( outfile(None, '.pdf') )
 
 
 
@@ -205,4 +259,11 @@ if __name__ == '__main__':
         hist.Draw('EP SAME')
     leg_sep = MyLegend_Sep(hists)
     leg_sep.Draw()
-    canv.SaveAs('h_photonCrossSection_sep.pdf')
+    canv.SaveAs( outfile('sep', '.pdf') )
+
+    import csv
+    f = open( outfile(None, '.csv'),'w')
+    csvwritter = csv.DictWriter(f, fieldnames=value_output[0].keys())
+    csvwritter.writeheader()
+    csvwritter.writerows(value_output)
+    f.close()
