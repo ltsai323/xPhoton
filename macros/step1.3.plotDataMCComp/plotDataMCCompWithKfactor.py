f8c4d3b020030b371a6832ea7d6a17a5decf1981
ltsai323 <johnson20050@gmail.com>
2024-09-03 15:56:01 +0800

    finialize cut

--- macros/step1.3.plotDataMCComp/plotDataMCCompWithKfactor.py
+++ macros/step1.3.plotDataMCComp/plotDataMCCompWithKfactor.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 import ROOT
-debug_mode = True
+debug_mode = False
+DEBUG_EVENT = 1000
+full_mode = False
+def BUG(mesg):
+    if debug_mode:
+        print(f'[BUG] {mesg}')
 
 
 def draw(inTREE:ROOT.TTree,
@@ -28,9 +33,12 @@ def event_cut( additionalCUT:str='1'):
     basic_cut = '&&'.join(['phoFillIdx==0', 'eleVeto==1', 'jetPt>30.', 'fabs(jetEta)<2.4', 'jetPUIDbit == 7', 'jetID==1']) # jetID == 1 is loose ID
     barrel_pho = '&&'.join(['fabs(recoSCEta)<1.4442'                       , 'HoverE<0.08', 'sieieFull5x5 < 0.012', 'phoIsoRaw<15.', 'chWorstIso<15.'])
     endcap_pho = '&&'.join(['fabs(recoSCEta)>1.566' , 'fabs(recoSCEta)<2.5', 'HoverE<0.05', 'sieieFull5x5 < 0.027', 'phoIsoRaw<15.', 'chWorstIso<15.'])
+    all_pho = f'( ({barrel_pho}) || ({endcap_pho}) )'
 
 
-    return all_cuts( basic_cut, barrel_pho, additionalCUT )
+    #return all_cuts( basic_cut, barrel_pho, additionalCUT )
+    #return all_cuts( basic_cut, endcap_pho, additionalCUT )
+    return all_cuts( basic_cut, all_pho, additionalCUT )
 
 BINNING_PHOPT = (100, 190.,1000.)
 BINNING_PHOETA= (40, -2.5, 2.5)
@@ -38,33 +46,52 @@ BINNING_PHOPHI= (40,-4.,4.)
 BINNING_CHISO = (40, 0., 15.)
 BINNING_N_VTX = (50, 0., 50.)
 BINNING_JETPT = (100, 0., 1000.)
+BINNING_SCORE = (20, 0.,1,)
 
-def DrawData(inTREE):
+def DrawData(inTREE, outTAG, additionalCUT:str = '1'):
     nTEMP = 'data_%s'
-    outTAG = 'expdata'
     evtWEIGHT = '1'
-    evtCUT = event_cut( 'recoPtCalib>190 && (((phoFiredTrgs>>7)&1)==1)')
+    evtCUT = event_cut( '&&'.join([additionalCUT, 'recoPtCalib>190 && (((phoFiredTrgs>>7)&1)==1)']) )
 
     hists = {}
-    if debug_mode:
-        hists['phoEta'] = draw(inTREE,'recoEta'    ,outTAG,'phoEta', evtCUT, evtWEIGHT, BINNING_PHOETA)
-        return hists
     hists['phoPt' ] = draw(inTREE,'recoPtCalib',outTAG,'phoPt' , evtCUT, evtWEIGHT, BINNING_PHOPT )
     hists['phoEta'] = draw(inTREE,'recoEta'    ,outTAG,'phoEta', evtCUT, evtWEIGHT, BINNING_PHOETA)
+    hists['phoETA'] = draw(inTREE,'recoSCEta'  ,outTAG,'phoETA', evtCUT, evtWEIGHT, BINNING_PHOETA)
     hists['phoPhi'] = draw(inTREE,'recoPhi'    ,outTAG,'phoPhi', evtCUT, evtWEIGHT, BINNING_PHOPHI)
     hists['chiso' ] = draw(inTREE,'chIsoRaw'   ,outTAG,'chiso' , evtCUT, evtWEIGHT, BINNING_CHISO )
     hists['nVtx'  ] = draw(inTREE,'nVtx'       ,outTAG,'nVtx'  , evtCUT, evtWEIGHT, BINNING_N_VTX )
     hists['jetPt' ] = draw(inTREE,'jetPt'      ,outTAG,'jetPt' , evtCUT, evtWEIGHT, BINNING_JETPT )
+
+    hists['DeepFlavour_bScore'] = draw(inTREE, 'DeepFlavour.bScore',outTAG,'DeepFlavour_bScore' , evtCUT, evtWEIGHT, BINNING_SCORE)
+    hists['DeepFlavour_CvsL'  ] = draw(inTREE, 'DeepFlavour.CvsL'  ,outTAG,'DeepFlavour_CvsL'   , evtCUT, evtWEIGHT, BINNING_SCORE)
+    hists['DeepFlavour_CvsB'  ] = draw(inTREE, 'DeepFlavour.CvsB'  ,outTAG,'DeepFlavour_CvsB'   , evtCUT, evtWEIGHT, BINNING_SCORE)
+    hists['DeepCSV_bScore'    ] = draw(inTREE, 'DeepCSV.bScore'    ,outTAG,'DeepCSV_bScore'     , evtCUT, evtWEIGHT, BINNING_SCORE)
+    hists['DeepCSV_CvsL'      ] = draw(inTREE, 'DeepCSV.CvsL'      ,outTAG,'DeepCSV_CvsL'       , evtCUT, evtWEIGHT, BINNING_SCORE)
+    hists['DeepCSV_CvsB'      ] = draw(inTREE, 'DeepCSV.CvsB'      ,outTAG,'DeepCSV_CvsB'       , evtCUT, evtWEIGHT, BINNING_SCORE)
+    return hists
+def DrawMC_DeepFlavour(inTREE, outTAG,additionalCUT:str = '1',  evtWEIGHT:str='1'):
+    evtCUT = event_cut( '&&'.join([additionalCUT, 'recoPt>190.']) )
+
+    hists = {}
+    hists['DeepFlavour_bScore'] = draw(inTREE, 'DeepFlavour.bScore',outTAG,'DeepFlavour_bScore' , evtCUT, evtWEIGHT, BINNING_SCORE)
+    hists['DeepFlavour_CvsL'  ] = draw(inTREE, 'DeepFlavour.CvsL'  ,outTAG,'DeepFlavour_CvsL'   , evtCUT, evtWEIGHT, BINNING_SCORE)
+    hists['DeepFlavour_CvsB'  ] = draw(inTREE, 'DeepFlavour.CvsB'  ,outTAG,'DeepFlavour_CvsB'   , evtCUT, evtWEIGHT, BINNING_SCORE)
+    return hists
+def DrawMC_DeepCSV(inTREE, outTAG,additionalCUT:str = '1',  evtWEIGHT:str='1'):
+    evtCUT = event_cut( '&&'.join([additionalCUT, 'recoPt>190.']) )
+
+    hists = {}
+    hists['DeepCSV_bScore'    ] = draw(inTREE, 'DeepCSV.bScore'    ,outTAG,'DeepCSV_bScore'     , evtCUT, evtWEIGHT, BINNING_SCORE)
+    hists['DeepCSV_CvsL'      ] = draw(inTREE, 'DeepCSV.CvsL'      ,outTAG,'DeepCSV_CvsL'       , evtCUT, evtWEIGHT, BINNING_SCORE)
+    hists['DeepCSV_CvsB'      ] = draw(inTREE, 'DeepCSV.CvsB'      ,outTAG,'DeepCSV_CvsB'       , evtCUT, evtWEIGHT, BINNING_SCORE)
     return hists
 def DrawMC(inTREE, outTAG,additionalCUT:str = '1',  evtWEIGHT:str='1'):
     evtCUT = event_cut( '&&'.join([additionalCUT, 'recoPt>190.']) )
 
     hists = {}
-    if debug_mode:
-        hists['phoEta'] = draw(inTREE,'recoEta'    ,outTAG,'phoEta', evtCUT, evtWEIGHT, BINNING_PHOETA)
-        return hists
     hists['phoPt' ] = draw(inTREE,'recoPt'     ,outTAG,'phoPt' , evtCUT, evtWEIGHT, BINNING_PHOPT )
     hists['phoEta'] = draw(inTREE,'recoEta'    ,outTAG,'phoEta', evtCUT, evtWEIGHT, BINNING_PHOETA)
+    hists['phoETA'] = draw(inTREE,'recoSCEta'  ,outTAG,'phoETA', evtCUT, evtWEIGHT, BINNING_PHOETA)
     hists['phoPhi'] = draw(inTREE,'recoPhi'    ,outTAG,'phoPhi', evtCUT, evtWEIGHT, BINNING_PHOPHI)
     hists['chiso' ] = draw(inTREE,'chIsoRaw'   ,outTAG,'chiso' , evtCUT, evtWEIGHT, BINNING_CHISO )
     hists['nVtx'  ] = draw(inTREE,'nVtx'       ,outTAG,'nVtx'  , evtCUT, evtWEIGHT, BINNING_N_VTX )
@@ -76,67 +103,115 @@ class TFileAndTree:
         self.tfile = ROOT.TFile.Open(inFILEname)
         ttree = self.tfile.Get('t')
         from testmodule_smalltree import GetSmallTree
-        self.ttree = GetSmallTree(ttree, 1000) if debug_mode else ttree
+        self.ttree = GetSmallTree(ttree, DEBUG_EVENT) if debug_mode else ttree
 
 def MainFunc():
     datafile = TFileAndTree('/home/ltsai/ReceivedFile/GJet/latestsample/UL2016PostVFP/data.root'         )
     fakefile = TFileAndTree('/home/ltsai/ReceivedFile/GJet/latestsample/UL2016PostVFP/qcd.madgraph.root' )
-    signfile = TFileAndTree('/home/ltsai/ReceivedFile/GJet/latestsample/UL2016PostVFP/sig.pythia.root'   )
+    #signfile = TFileAndTree('/home/ltsai/ReceivedFile/GJet/latestsample/UL2016PostVFP/sig.pythia.root'   )
+    signfile = TFileAndTree('/home/ltsai/ReceivedFile/GJet/latestsample/UL2016PostVFP/sig.madgraph.root' )
 
 
+    strict_C_selection = 'jetSubVtx3DVal/jetSubVtx3DErr>8. && jetSubVtxNtrks>2'
+    drawing_hists = []
+    drawing_hists.append( DrawData(datafile.ttree, 'expdata') )
+    drawing_hists.append( DrawData(datafile.ttree, 'expdataStrictC', strict_C_selection) )
+    # all weights
+    drawing_hists.append( DrawMC  ( signfile.ttree, 'signALL',
+            'isMatched==1', 'mcweight *puwei * scalefactor_photon* jetP4Smear') )
 
-    expdata = DrawData( datafile.ttree )
-    hsignA = DrawMC  ( signfile.ttree, 'signALL', # all weights
-            'isMatched==1', 'mcweight *puwei * scalefactor_photon* jetP4Smear')
-    hsign0 = DrawMC  ( signfile.ttree, 'sign0', # only luminosity weight
-            'isMatched==1', 'mcweight')
-    hsign1 = DrawMC  ( signfile.ttree, 'sign1', # lunminosity wieght + puwei
-            'isMatched==1', 'mcweight *puwei')
-    hsign2 = DrawMC  ( signfile.ttree, 'sign2', # lumi + pho sf
-            'isMatched==1', 'mcweight * scalefactor_photon')
-    hsign3 = DrawMC  ( signfile.ttree, 'sign3', # lumi + jetP4Smear
-            'isMatched==1', 'mcweight * jetP4Smear')
+    drawing_hists.append( DrawMC_DeepCSV    ( signfile.ttree, 'signDeepCSVOrig',
+            'isMatched==1', 'mcweight *puwei * scalefactor_photon* jetP4Smear ') )
+    drawing_hists.append( DrawMC_DeepCSV    ( signfile.ttree, 'signDeepCSVCalib',
+            'isMatched==1', 'mcweight *puwei * scalefactor_photon* jetP4Smear * DeepCSV.ctagWeight.central'    ) )
+    drawing_hists.append( DrawMC_DeepCSV    ( signfile.ttree, 'signDeepCSVOrigStrictC',
+            '&&'.join(['isMatched==1',strict_C_selection]), 'mcweight *puwei * scalefactor_photon* jetP4Smear ') )
+    drawing_hists.append( DrawMC_DeepCSV    ( signfile.ttree, 'signDeepCSVCalibStrictC',
+            '&&'.join(['isMatched==1',strict_C_selection]), 'mcweight *puwei * scalefactor_photon* jetP4Smear * DeepCSV.ctagWeight.central'    ) )
 
-    hfakeA = DrawMC  ( fakefile.ttree, 'fakeALL', # all weights
-            'isMatched!=1', 'mcweight *puwei * scalefactor_photon* jetP4Smear')
-    hfake0 = DrawMC  ( fakefile.ttree, 'fake0', # only luminosity weight
-            'isMatched!=1', 'mcweight')
-    hfake1 = DrawMC  ( fakefile.ttree, 'fake1', # lunminosity wieght + puwei
-            'isMatched!=1', 'mcweight *puwei')
-    hfake2 = DrawMC  ( fakefile.ttree, 'fake2', # lumi + pho sf
-            'isMatched!=1', 'mcweight *scalefactor_photon')
-    hfake3 = DrawMC  ( fakefile.ttree, 'fake3', # lumi + jetP4Smear
-            'isMatched!=1', 'mcweight * jetP4Smear')
-    hfake4 = DrawMC  ( fakefile.ttree, 'fake4', # lumi + pass max pu
-            'isMatched!=1&&passMaxPUcut', 'mcweight * weight_passMaxPUcut')
+    if full_mode:
+        drawing_hists.append( DrawMC_DeepFlavour( signfile.ttree, 'signDeepFlavourOrig',
+                'isMatched==1', 'mcweight *puwei * scalefactor_photon* jetP4Smear ') )
+        drawing_hists.append( DrawMC_DeepFlavour( signfile.ttree, 'signDeepFlavourCalib',
+                'isMatched==1', 'mcweight *puwei * scalefactor_photon* jetP4Smear * DeepFlavour.ctagWeight.central') )
+        drawing_hists.append( DrawMC_DeepFlavour( signfile.ttree, 'signDeepFlavourOrigStrictC',
+                '&&'.join(['isMatched==1',strict_C_selection]), 'mcweight *puwei * scalefactor_photon* jetP4Smear ') )
+        drawing_hists.append( DrawMC_DeepFlavour( signfile.ttree, 'signDeepFlavourCalibStrictC',
+                '&&'.join(['isMatched==1',strict_C_selection]), 'mcweight *puwei * scalefactor_photon* jetP4Smear * DeepFlavour.ctagWeight.central') )
 
-    #canv = ROOT.TCanvas('c1','', 800,800)
-    #canv.SetLogy()
-    #h["phoPhi"].Draw("EP")
-    #canv.SaveAs("hi.png")
+    if full_mode:
+        # only luminosity weight
+        drawing_hists.append( DrawMC  ( signfile.ttree, 'sign0',
+                'isMatched==1', 'mcweight') )
+        # lunminosity wieght + puwei
+        drawing_hists.append( DrawMC  ( signfile.ttree, 'sign1',
+                'isMatched==1', 'mcweight *puwei') )
+         # lumi + pho sf
+        drawing_hists.append( DrawMC  ( signfile.ttree, 'sign2',
+                'isMatched==1', 'mcweight * scalefactor_photon') )
+         # lumi + jetP4Smear
+        drawing_hists.append( DrawMC  ( signfile.ttree, 'sign3',
+                'isMatched==1', 'mcweight * jetP4Smear') )
+
+    # all weights
+    drawing_hists.append( DrawMC  ( fakefile.ttree, 'fakeALL',
+            'isMatched!=1', 'mcweight *puwei * scalefactor_photon* jetP4Smear') )
+
+    drawing_hists.append( DrawMC_DeepCSV    ( fakefile.ttree, 'fakeDeepCSVOrig',
+            'isMatched!=1', 'mcweight *puwei * scalefactor_photon* jetP4Smear ') )
+    drawing_hists.append( DrawMC_DeepCSV    ( fakefile.ttree, 'fakeDeepCSVCalib',
+            'isMatched!=1', 'mcweight *puwei * scalefactor_photon* jetP4Smear * DeepCSV.ctagWeight.central'    ) )
+    drawing_hists.append( DrawMC_DeepCSV    ( fakefile.ttree, 'fakeDeepCSVOrigStrictC',
+            '&&'.join(['isMatched!=1',strict_C_selection]), 'mcweight *puwei * scalefactor_photon* jetP4Smear ') )
+    drawing_hists.append( DrawMC_DeepCSV    ( fakefile.ttree, 'fakeDeepCSVCalibStrictC',
+            '&&'.join(['isMatched!=1',strict_C_selection]), 'mcweight *puwei * scalefactor_photon* jetP4Smear * DeepCSV.ctagWeight.central'    ) )
+
+    if full_mode:
+        drawing_hists.append( DrawMC_DeepFlavour( fakefile.ttree, 'fakeDeepFlavourOrig',
+                'isMatched!=1', 'mcweight *puwei * scalefactor_photon* jetP4Smear ') )
+        drawing_hists.append( DrawMC_DeepFlavour( fakefile.ttree, 'fakeDeepFlavourCalib',
+                'isMatched!=1', 'mcweight *puwei * scalefactor_photon* jetP4Smear * DeepFlavour.ctagWeight.central') )
+        drawing_hists.append( DrawMC_DeepFlavour( fakefile.ttree, 'fakeDeepFlavourOrigStrictC',
+                '&&'.join(['isMatched!=1',strict_C_selection]), 'mcweight *puwei * scalefactor_photon* jetP4Smear ') )
+        drawing_hists.append( DrawMC_DeepFlavour( fakefile.ttree, 'fakeDeepFlavourCalibStrictC',
+                '&&'.join(['isMatched!=1',strict_C_selection]), 'mcweight *puwei * scalefactor_photon* jetP4Smear * DeepFlavour.ctagWeight.central') )
+
+    if full_mode:
+        # only luminosity weight
+        drawing_hists.append( DrawMC  ( fakefile.ttree, 'fake0',
+                'isMatched!=1', 'mcweight') )
+         # lunminosity wieght + puwei
+        drawing_hists.append( DrawMC  ( fakefile.ttree, 'fake1',
+                'isMatched!=1', 'mcweight *puwei') )
+         # lumi + pho sf
+        drawing_hists.append( DrawMC  ( fakefile.ttree, 'fake2',
+                'isMatched!=1', 'mcweight *scalefactor_photon') )
+         # lumi + jetP4Smear
+        drawing_hists.append( DrawMC  ( fakefile.ttree, 'fake3',
+                'isMatched!=1', 'mcweight * jetP4Smear') )
+         # lumi + pass max pu
+        drawing_hists.append( DrawMC  ( fakefile.ttree, 'fake4',
+                'isMatched!=1&&passMaxPUcut', 'mcweight * weight_passMaxPUcut') )
+
+
 
     outfile = ROOT.TFile('dataMCcomp.root', 'recreate')
     def writeTo(outFILE, histDICT):
-        dirName = histDICT['phoEta'].GetName().split('_')[0]
+        try:
+            first_key = list(histDICT.keys())[0]
+            BUG(f'Writing directory "{ first_key }" to ROOT file.')
+        except IndexError as e:
+            raise IndexError(f'writeTo() said nothing inside this histogram dictionary. Terminate process... (histDICT = {histDICT})')
+        dirName = histDICT[first_key].GetName().split('_')[0]
+        BUG(f'Creating directory "{ dirName }"')
         tdir = outFILE.mkdir(dirName)
         tdir.cd()
         for objname, hist in histDICT.items():
             hist.SetName(objname)
             hist.Write()
-    writeTo(outfile, expdata)
 
-    writeTo(outfile, hsignA)
-    writeTo(outfile, hsign0)
-    writeTo(outfile, hsign1)
-    writeTo(outfile, hsign2)
-    writeTo(outfile, hsign3)
-
-    writeTo(outfile, hfakeA)
-    writeTo(outfile, hfake0)
-    writeTo(outfile, hfake1)
-    writeTo(outfile, hfake2)
-    writeTo(outfile, hfake3)
-    writeTo(outfile, hfake4)
+    for drawn_hists in drawing_hists:
+        writeTo(outfile, drawn_hists)
 
     outfile.Close()
 
