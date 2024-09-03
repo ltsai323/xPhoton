099e3068688d5d6e9d7b1504526ac376fe9928ba
ltsai323 <johnson20050@gmail.com>
2024-09-03 15:32:08 +0800

    sync

--- macros/step4.Fitting_poweredByHiggsCombine/massiverun.sh
+++ macros/step4.Fitting_poweredByHiggsCombine/massiverun.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env sh
-function the_exit()
-{ echo -e "$1";exit; }
-function link_pt_bin_definition()
-{ unlink py_pt_ranges_definition.py; ln -s $1 py_pt_ranges_definition.py|| the_exit "link failed to $1"; }
-function main_func()
-{
-    storageDIR=$1
-    outLABEL=$2
-
-    /bin/rm -rf tmp_BDTFit*
-    inputfile=${storageDIR}/${outLABEL}/makehisto.root
-    outfolder=${storageDIR}/${outLABEL}/
-
-    echo input file    : $inputfile
-    echo output folder : $outfolder
-    sh stepALL_higgscombine.sh $outLABEL $inputfile || the_exit "=====\n-----\nhiggs combine running failed\n-----\n======"
-    mv $outLABEL/* $outfolder
-    rmdir $outLABEL
-}
-
-storage_dir=/wk_cms3/ltsai/wk_cms/ltsai/ReceivedFile/GJet/latestsample/UL2016PreVFP/makehistos
+source ./runfrag.sh
+storage_dir=/wk_cms3/ltsai/wk_cms/ltsai/ReceivedFile/GJet/latestsample/UL2016PostVFP/makehistos
 
 link_pt_bin_definition ../py_pt_ranges_definition_testmodeOrig.py
 main_func $storage_dir DeepCSV_gjetMadgraph_cutIdx0_mergeBin_Orig
