06ae778939f2ba092d1e1ccf9b8aa2c377c6571d
ltsai323 <johnson20050@gmail.com>
2024-09-03 15:33:17 +0800

    new run script

--- macros/step41.CTagSimultaneousFit_higgsCombine/check.sh
+++ macros/step41.CTagSimultaneousFit_higgsCombine/check.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env sh
-function exec_in_line()
-{
-    outLABEL=$1; 
-    inDIR=/home/ltsai/ReceivedFile/GJet/latestsample/UL2016PreVFP/makehistos/;
-    sh stepALL_higgscombine.sh $outLABEL $inDIR/$outLABEL | tee logALL_$outLABEL;
-    mv logALL_$outLABEL $outLABEL/;
-    /bin/rm -r cmd_history_step2_*;
-    /bin/rm -r tmp_CTag_SimulFit*;
-}
-function link_pt_bin_definition()
-{ unlink py_pt_ranges_definition.py; ln -s $CMSSW_BASE/src/xPhoton/MyCommonTools/python/ptbin_definitions/$1 py_pt_ranges_definition.py|| the_exit "link failed to $1"; }
+source ./runfrag.sh
 
-
-link_pt_bin_definition py_pt_ranges_definition_testmode5.py
-exec_in_line DeepCSV_gjetPythia_cutIdx4_mergeBin_5
-exec_in_line DeepCSV_gjetPythia_cutIdx5_mergeBin_5
-exec_in_line DeepFlavour_gjetPythia_cutIdx4_mergeBin_5
-exec_in_line DeepFlavour_gjetPythia_cutIdx5_mergeBin_5
+link_pt_bin_definition py_pt_ranges_definition_testmode9.py
+exec_in_line DeepCSV_gjetMadgraph_cutIdx0_mergeBin_9
+bark.sh 'Finished Job 1/3' 'DeepCSV_gjetMadgraph_cutIdx0_mergeBin_9'
+exec_in_line DeepCSV_gjetMadgraph_cutIdx4_mergeBin_9
+bark.sh 'Finished Job 2/3' 'DeepCSV_gjetMadgraph_cutIdx4_mergeBin_9'
+exec_in_line DeepCSV_gjetMadgraph_cutIdx5_mergeBin_9
+bark.sh 'Finished Job 3/3' 'DeepCSV_gjetMadgraph_cutIdx5_mergeBin_9'
+#exec_in_line DeepCSV_gjetPythia_cutIdx5_mergeBin_5
+#exec_in_line DeepFlavour_gjetPythia_cutIdx4_mergeBin_5
+#exec_in_line DeepFlavour_gjetPythia_cutIdx5_mergeBin_5
+bark.sh 'Step41 Finished' '~'
---
+++ macros/step41.CTagSimultaneousFit_higgsCombine/runfrag.sh
@@ -0,0 +1,18 @@
+
+function test_in_line()
+{
+    outLABEL=$1; 
+    inDIR=/home/ltsai/ReceivedFile/GJet/latestsample/UL2016PostVFP/makehistos/;
+    sh steptest_higgscombine.sh $outLABEL $inDIR/$outLABEL ;
+}
+function exec_in_line()
+{
+    outLABEL=$1; 
+    inDIR=/home/ltsai/ReceivedFile/GJet/latestsample/UL2016PostVFP/makehistos/;
+    sh steptest_higgscombine.sh $outLABEL $inDIR/$outLABEL | tee logALL_$outLABEL;
+    mv logALL_$outLABEL $outLABEL/;
+    /bin/rm -r cmd_history_step2_*;
+    /bin/rm -r tmp_CTag_SimulFit*;
+}
+function link_pt_bin_definition()
+{ unlink py_pt_ranges_definition.py; ln -s $CMSSW_BASE/src/xPhoton/MyCommonTools/python/ptbin_definitions/$1 py_pt_ranges_definition.py|| the_exit "link failed to $1"; }
